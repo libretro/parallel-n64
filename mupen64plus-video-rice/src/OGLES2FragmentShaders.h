21494e2bc3603a69613e067c5bdcf447ecd32dc3
meancoot <jasonfetters@gmail.com>
2013-06-07 21:27:37 -0400

    Mode video-rice deletion

--- mupen64plus-video-rice/src/OGLES2FragmentShaders.h
+++ mupen64plus-video-rice/src/OGLES2FragmentShaders.h
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "OGLCombiner.h"
 #include "OGLExtCombiner.h"
-#include "GeneralCombiner.h"
 
 typedef struct {
     uint32  dwMux0;
