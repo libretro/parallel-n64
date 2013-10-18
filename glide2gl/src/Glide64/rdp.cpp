libretro/parallel-n64

commit 70fabfb8cf06f3d8a077d8e117ee4ccbb2c86cfa

(Glide2GL) Glide64 - fullscreen always assumed on

glide2gl/src/Glide64/rdp.cpp
@@ -408,8 +408,6 @@ static void DrawPartFrameBufferToScreen()
 
 static void CopyFrameBuffer (GrBuffer_t buffer = GR_BUFFER_BACKBUFFER)
 {
-  if (!fullscreen)
-    return;
   FRDP ("CopyFrameBuffer: %08lx... ", rdp.cimg);
 
   // don't bother to write the stuff in asm... the slow part is the read from video card,
@@ -602,7 +600,6 @@ EXPORT void CALL ProcessDList(void)
   VLOG ("ProcessDList ()\n");
 #endif
 
-  if (!fullscreen)
   {
     // Set an interrupt to allow the game to continue
     *gfx.MI_INTR_REG |= 0x20;
@@ -630,9 +627,6 @@ EXPORT void CALL ProcessDList(void)
   }
 
   if (exception)
-    return;
-
-  if (!fullscreen && !settings.run_in_window)
     return;
 
   // Clear out the RDP log
@@ -1392,7 +1386,6 @@ static void rdp_texrect()
       apply_shade_mods (&vptr[i]);
     }
 
-    if (fullscreen)
     {
       if (rdp.fog_mode >= RDP::fog_blend)
       {
@@ -1412,10 +1405,6 @@ static void rdp_texrect()
 
       grDrawVertexArrayContiguous (GR_TRIANGLE_STRIP, n_vertices, vptr, sizeof(VERTEX));
 
-      rdp.tri_n += 2;
-    }
-    else
-    {
       rdp.tri_n += 2;
     }
 
@@ -2226,7 +2215,6 @@ static void rdp_fillrect()
   if ((rdp.cimg == rdp.zimg) || (fb_emulation_enabled && rdp.frame_buffers[rdp.ci_count-1].status == ci_zimg) || pd_multiplayer)
   {
     LRDP("Fillrect - cleared the depth buffer\n");
-    if (fullscreen)
     {
       if (!(settings.hacks&hack_Hyperbike) || rdp.ci_width > 64) //do not clear main depth buffer for aux depth buffers
       {
@@ -2286,8 +2274,7 @@ static void rdp_fillrect()
   }
 
   // Update scissor
-  if (fullscreen)
-    update_scissor ();
+  update_scissor ();
 
   if (settings.decrease_fillrect_edge && rdp.cycle_mode == 0)
   {
@@ -2312,7 +2299,6 @@ static void rdp_fillrect()
 
   FRDP (" - %d, %d, %d, %d\n", s_ul_x, s_ul_y, s_lr_x, s_lr_y);
 
-  if (fullscreen)
   {
     grFogMode (GR_FOG_DISABLE);
 
@@ -2395,10 +2381,6 @@ static void rdp_fillrect()
       grDrawTriangle2(&v[0], &v[2], &v[1], &v[2], &v[3], &v[1]);
 
       rdp.tri_n += 2;
-  }
-  else
-  {
-    rdp.tri_n += 2;
   }
 }
 
@@ -2554,7 +2536,6 @@ static void RestoreScale()
   rdp.view_trans[1] *= rdp.scale_y;
   rdp.update |= UPDATE_VIEWPORT | UPDATE_SCISSOR;
   //*
-  if (fullscreen)
   {
     grDepthMask (FXFALSE);
     grBufferClear (0, 0, 0xFFFF);
@@ -3655,7 +3636,6 @@ void lle_triangle(uint32_t w1, uint32_t w2, int shade, int texture, int zbuffer,
     }
   }
 
-  if (fullscreen)
   {
     update ();
     for (int k = 0; k < nbVtxs-1; k++)

glide2gl/src/Glide64/Combine.cpp
@@ -15567,12 +15567,9 @@ void Combine ()
   if (cmb.cmb_ext_use || cmb.tex_cmb_ext_use)
   {
     //have to draw something to allow use of standard combine functions
-    if (fullscreen)
-    {
-      VERTEX v;
-      memset(&v,0,sizeof(v));
-      grDrawPoint(&v);
-    }
+     VERTEX v;
+     memset(&v,0,sizeof(v));
+     grDrawPoint(&v);
     cmb.cmb_ext_use = 0;
     cmb.tex_cmb_ext_use = 0;
   }
@@ -15728,7 +15725,6 @@ void Combine ()
   //*/
   rdp.tex = cmb.tex;
 
-  if (fullscreen)
   {
     TBUFF_COLOR_IMAGE * aTBuff[2] = {0, 0};
     if (rdp.aTBuffTex[0])

glide2gl/src/Glide64/Main.cpp
@@ -1022,22 +1022,6 @@ EXPORT void CALL ReadScreen2(void *dest, int *width, int *height, int front)
   if (dest)
   {
     uint8_t * line = (uint8_t*)dest;
-    if (!fullscreen)
-    {
-      for (uint32_t y=0; y<settings.res_y; y++)
-      {
-        for (uint32_t x=0; x<settings.res_x; x++)
-        {
-          line[x*3] = 0x20;
-          line[x*3+1] = 0x7f;
-          line[x*3+2] = 0x40;
-        }
-      }
-      // LOG ("ReadScreen. not in the fullscreen!\n");
-      WARNLOG("[Glide64] Cannot save screenshot in windowed mode?\n");
-
-      return;
-    }
 
   GrLfbInfo_t info;
   info.size = sizeof(GrLfbInfo_t);
@@ -1455,13 +1439,10 @@ void drawViRegBG()
 
 void DrawFrameBuffer ()
 {
-  if (fullscreen)
-  {
-    grDepthMask (FXTRUE);
-    grColorMask (FXTRUE, FXTRUE);
-    grBufferClear (0, 0, 0xFFFF);
-    drawViRegBG();
-  }
+   grDepthMask (FXTRUE);
+   grColorMask (FXTRUE, FXTRUE);
+   grBufferClear (0, 0, 0xFFFF);
+   drawViRegBG();
 }
 
 extern "C" {
@@ -1484,7 +1465,7 @@ EXPORT void CALL UpdateScreen (void)
 #endif
 
   uint32_t width = (*gfx.VI_WIDTH_REG) << 1;
-  if (fullscreen && (*gfx.VI_ORIGIN_REG  > width))
+  if (*gfx.VI_ORIGIN_REG  > width)
     update_screen_count++;
   uint32_t limit = (settings.hacks&hack_Lego) ? 15 : 30;
   if ((settings.frame_buffer&fb_cpu_write_hack) && (update_screen_count > limit) && (rdp.last_bg == 0))
@@ -1576,7 +1557,6 @@ void newSwapBuffers()
   if (settings.frame_buffer & fb_read_back_to_screen)
     DrawWholeFrameBufferToScreen();
 
-  if (fullscreen)
   {
     if (fb_hwfbe_enabled && !(settings.hacks&hack_RE2) && !evoodoo)
       grAuxBufferExt( GR_BUFFER_AUXBUFFER );
@@ -1590,15 +1570,6 @@ void newSwapBuffers()
         grDepthMask (FXTRUE);
       grBufferClear (0, 0, 0xFFFF);
     }
-    /* //let the game to clear the buffers
-    else
-    {
-    grDepthMask (FXTRUE);
-    grColorMask (FXFALSE, FXFALSE);
-    grBufferClear (0, 0, 0xFFFF);
-    grColorMask (FXTRUE, FXTRUE);
-    }
-    */
   }
 
   if (settings.frame_buffer & fb_read_back_to_screen2)

glide2gl/src/Glide64/TexBuffer.cpp
@@ -210,7 +210,6 @@ static TBUFF_COLOR_IMAGE * AllocateTextureBuffer(COLOR_IMAGE & cimage)
 int OpenTextureBuffer(COLOR_IMAGE & cimage)
 {
   FRDP("OpenTextureBuffer. cur_tex_buf: %d, addr: %08lx, width: %d, height: %d", rdp.cur_tex_buf, cimage.addr, cimage.width, cimage.height);
-  if (!fullscreen) return FALSE;
 
   int found = FALSE, search = TRUE;
   TBUFF_COLOR_IMAGE *texbuf = 0;
@@ -419,7 +418,7 @@ static GrTextureFormat_t TexBufSetupCombiner(int force_rgb = FALSE)
 
 int CloseTextureBuffer(int draw)
 {
-  if (!fullscreen || !rdp.cur_image)
+  if (!rdp.cur_image)
   {
     LRDP("CloseTextureBuffer KO\n");
     return FALSE;
@@ -471,8 +470,6 @@ int CloseTextureBuffer(int draw)
 
 int CopyTextureBuffer(COLOR_IMAGE & fb_from, COLOR_IMAGE & fb_to)
 {
-  if (!fullscreen)
-    return FALSE;
   FRDP("CopyTextureBuffer from %08x to %08x\n", fb_from.addr, fb_to.addr);
   if (rdp.cur_image)
   {
@@ -536,8 +533,6 @@ int CopyTextureBuffer(COLOR_IMAGE & fb_from, COLOR_IMAGE & fb_to)
 
 int CopyDepthBuffer()
 {
-  if (!fullscreen)
-    return FALSE;
   LRDP("CopyDepthBuffer. ");
   float bound = 1024.0f;
   GrLOD_t LOD = GR_LOD_LOG2_1024;
@@ -591,7 +586,7 @@ int CopyDepthBuffer()
 
 int SwapTextureBuffer()
 {
-  if (!fullscreen || !rdp.tbuff_tex)
+  if (!rdp.tbuff_tex)
     return FALSE;
   LRDP("SwapTextureBuffer.");
   COLOR_IMAGE ci;

glide2gl/src/Glide64/TexCache.cpp
@@ -493,7 +493,6 @@ void GetTexInfo (int id, int tile)
 
 int ChooseBestTmu (int tmu1, int tmu2)
 {
-  if (!fullscreen) return tmu1;
   return 0;
 }
 
@@ -670,7 +669,6 @@ void TexCache ()
   rdp.t1 = tmu_1;
 
   // SET the combiner
-  if (fullscreen)
   {
     if (rdp.allow_combine)
     {
@@ -765,28 +763,22 @@ void TexCache ()
     if (aTBuff[0] && aTBuff[0]->cache)
     {
       LRDP(" | |- Hires tex T0 found in cache.\n");
-      if (fullscreen)
-      {
-        rdp.cur_cache[0] = aTBuff[0]->cache;
-        rdp.cur_cache[0]->last_used = frame_count;
-        rdp.cur_cache[0]->uses = rdp.debug_n;
-      }
+      rdp.cur_cache[0] = aTBuff[0]->cache;
+      rdp.cur_cache[0]->last_used = frame_count;
+      rdp.cur_cache[0]->uses = rdp.debug_n;
     }
     else if (tex_found[0][tmu_0] != -1)
     {
-      LRDP(" | |- T0 found in cache.\n");
-      if (fullscreen)
-      {
-        CACHE_LUT *cache = &rdp.cache[0][tex_found[0][0]];
-        rdp.cur_cache_n[0] = tex_found[0][tmu_0];
-        rdp.cur_cache[0] = cache;
-        rdp.cur_cache[0]->last_used = frame_count;
-        rdp.cur_cache[0]->uses = rdp.debug_n;
-        grTexSource (tmu_0,
-          (voodoo.tex_min_addr[tmu_0] + cache->tmem_addr),
-          GR_MIPMAPLEVELMASK_BOTH,
-          &cache->t_info);
-      }
+       LRDP(" | |- T0 found in cache.\n");
+       CACHE_LUT *cache = &rdp.cache[0][tex_found[0][0]];
+       rdp.cur_cache_n[0] = tex_found[0][tmu_0];
+       rdp.cur_cache[0] = cache;
+       rdp.cur_cache[0]->last_used = frame_count;
+       rdp.cur_cache[0]->uses = rdp.debug_n;
+       grTexSource (tmu_0,
+             (voodoo.tex_min_addr[tmu_0] + cache->tmem_addr),
+             GR_MIPMAPLEVELMASK_BOTH,
+             &cache->t_info);
     }
     else
       LoadTex (0, tmu_0);
@@ -795,35 +787,28 @@ void TexCache ()
   {
     if (aTBuff[1] && aTBuff[1]->cache)
     {
-      LRDP(" | |- Hires tex T1 found in cache.\n");
-      if (fullscreen)
-      {
-        rdp.cur_cache[1] = aTBuff[1]->cache;
-        rdp.cur_cache[1]->last_used = frame_count;
-        rdp.cur_cache[1]->uses = rdp.debug_n;
-      }
+       LRDP(" | |- Hires tex T1 found in cache.\n");
+       rdp.cur_cache[1] = aTBuff[1]->cache;
+       rdp.cur_cache[1]->last_used = frame_count;
+       rdp.cur_cache[1]->uses = rdp.debug_n;
     }
     else if (tex_found[1][tmu_1] != -1)
     {
-      LRDP(" | |- T1 found in cache.\n");
-      if (fullscreen)
-      {
-        CACHE_LUT *cache = &rdp.cache[0][tex_found[1][0]];
-        rdp.cur_cache_n[1] = tex_found[1][tmu_1];
-        rdp.cur_cache[1] = cache;
-        rdp.cur_cache[1]->last_used = frame_count;
-        rdp.cur_cache[1]->uses = rdp.debug_n;
-        grTexSource (tmu_1,
-          (voodoo.tex_min_addr[tmu_1] + cache->tmem_addr),
-          GR_MIPMAPLEVELMASK_BOTH,
-          &cache->t_info);
-      }
+       LRDP(" | |- T1 found in cache.\n");
+       CACHE_LUT *cache = &rdp.cache[0][tex_found[1][0]];
+       rdp.cur_cache_n[1] = tex_found[1][tmu_1];
+       rdp.cur_cache[1] = cache;
+       rdp.cur_cache[1]->last_used = frame_count;
+       rdp.cur_cache[1]->uses = rdp.debug_n;
+       grTexSource (tmu_1,
+             (voodoo.tex_min_addr[tmu_1] + cache->tmem_addr),
+             GR_MIPMAPLEVELMASK_BOTH,
+             &cache->t_info);
     }
     else
       LoadTex (1, tmu_1);
   }
 
-  if (fullscreen)
   {
     for (int i=0; i<2; i++)
     {
@@ -1551,7 +1536,6 @@ void LoadTex (int id, int tmu)
     cache->lod = lod;
     cache->aspect = aspect;
 
-    if (fullscreen)
     {
 #ifdef TEXTURE_FILTER // Hiroshi Morii <koolsmoky@users.sourceforge.net>
       if (settings.ghq_use)

glide2gl/src/Glide64/Util.cpp
@@ -1596,7 +1596,6 @@ static void render_tri (uint16_t linew, int old_interpolate)
   }
   }
   */
-  if (fullscreen)
   {
 
       //      VERTEX ** pv = rdp.vtx_buffer?(vtx_list2):(vtx_list1);
@@ -1683,9 +1682,7 @@ void update_scissor ()
     rdp.scissor.lr_y = (uint32_t)max(min((rdp.scissor_o.lr_y * rdp.scale_y + rdp.offset_y + 0.01f),settings.res_y),0);
     //grClipWindow specifies the hardware clipping window. Any pixels outside the clipping window are rejected.
     //Values are inclusive for minimum x and y values and exclusive for maximum x and y values.
-//    grClipWindow (rdp.scissor.ul_x?rdp.scissor.ul_x+1:0, rdp.scissor.ul_y?rdp.scissor.ul_y+1:0, rdp.scissor.lr_x, rdp.scissor.lr_y);
-    if (fullscreen)
-      grClipWindow (rdp.scissor.ul_x, rdp.scissor.ul_y, rdp.scissor.lr_x, rdp.scissor.lr_y);
+    grClipWindow (rdp.scissor.ul_x, rdp.scissor.ul_y, rdp.scissor.lr_x, rdp.scissor.lr_y);
     FRDP (" |- scissor - (%d, %d) -> (%d, %d)\n", rdp.scissor.ul_x, rdp.scissor.ul_y,
       rdp.scissor.lr_x, rdp.scissor.lr_y);
   }
@@ -1802,7 +1799,6 @@ void update ()
       rdp.update ^= UPDATE_TEXTURE;
   }
 
-  if (fullscreen)
   {
     // Z buffer
     if (rdp.update & UPDATE_ZBUF_ENABLED)
@@ -1992,7 +1988,6 @@ void update ()
   if (rdp.update & UPDATE_VIEWPORT)
   {
     rdp.update ^= UPDATE_VIEWPORT;
-    if (fullscreen)
     {
       float scale_x = (float)fabs(rdp.view_scale[0]);
       float scale_y = (float)fabs(rdp.view_scale[1]);

glide2gl/src/Glide64/ucode06.h
@@ -246,7 +246,7 @@ void DrawHiresDepthImage (const DRAWIMAGE & d)
 
 void DrawDepthImage (const DRAWIMAGE & d)
 {
-  if (!fullscreen || !fb_depth_render_enabled)
+  if (!fb_depth_render_enabled)
     return;
   if (d.imageH > d.imageW)
     return;
@@ -443,7 +443,6 @@ void DrawImage (DRAWIMAGE & d)
   if (rdp.cycle_mode == 2)
     rdp.allow_combine = 0;
 
-  if (fullscreen)
   {
     if (rdp.ci_width == 512 && !no_dlist)
       grClipWindow (0, 0, settings.scr_res_x, settings.scr_res_y);
@@ -549,8 +548,7 @@ void DrawImage (DRAWIMAGE & d)
             apply_shade_mods (&(v[s]));
           ConvertCoordsConvert (v, 4);
 
-          if (fullscreen)
-            grDrawVertexArrayContiguous (GR_TRIANGLE_STRIP, 4, v, sizeof(VERTEX));
+          grDrawVertexArrayContiguous (GR_TRIANGLE_STRIP, 4, v, sizeof(VERTEX));
 
           rdp.tri_n += 2;
       }
@@ -591,8 +589,6 @@ void DrawImage (DRAWIMAGE & d)
 
 void DrawHiresImage(DRAWIMAGE & d, int screensize = FALSE)
 {
-  if (!fullscreen)
-    return;
   TBUFF_COLOR_IMAGE *tbuff_tex = rdp.tbuff_tex;
   if (rdp.motionblur)
     rdp.tbuff_tex = &(rdp.texbufs[rdp.cur_tex_buf^1].images[0]);
@@ -957,7 +953,7 @@ static void uc6_draw_polygons (VERTEX v[4])
   }
   rdp.update |= UPDATE_ZBUF_ENABLED | UPDATE_VIEWPORT;
 
-  if (fullscreen && settings.fog && (rdp.flags & FOG_ENABLED))
+  if (settings.fog && (rdp.flags & FOG_ENABLED))
   {
     grFogMode (GR_FOG_WITH_TABLE_ON_FOGCOORD_EXT);
   }
@@ -1670,7 +1666,7 @@ void uc6_sprite2d ()
         }
         rdp.update |= UPDATE_ZBUF_ENABLED | UPDATE_VIEWPORT;
 
-        if (fullscreen && settings.fog && (rdp.flags & FOG_ENABLED))
+        if (settings.fog && (rdp.flags & FOG_ENABLED))
         {
           grFogMode (GR_FOG_WITH_TABLE_ON_FOGCOORD_EXT);
         }

commit 54e4677f5f17d3a64f2334eeb99166d9135af1d3

Fixed regression caused in commit 70fabfb8cf that caused Blast Corps to no longer progress past the company logo screen

glide2gl/src/Glide64/rdp.cpp
@@ -600,12 +600,6 @@ EXPORT void CALL ProcessDList(void)
   VLOG ("ProcessDList ()\n");
 #endif
 
-  {
-    // Set an interrupt to allow the game to continue
-    *gfx.MI_INTR_REG |= 0x20;
-    gfx.CheckInterrupts();
-  }
-
   if (reset)
   {
     reset = 0;