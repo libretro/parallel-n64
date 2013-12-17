8745b0c41141ef94a672ec24a4c854b1b588c661
twinaphex <libretro@gmail.com>
2013-12-17 21:54:54 +0100

    Update SDL.h with new performance counter callbacks

--- libretro/SDL.h
+++ libretro/SDL.h
@@ -8,8 +8,10 @@
 extern unsigned int FAKE_SDL_TICKS;
 
 extern retro_get_cpu_features_t perf_get_cpu_features_cb;
+extern retro_perf_get_counter_t perf_get_counter_cb;
 extern retro_perf_log_t perf_log_cb;
 extern retro_log_printf_t log_cb;
+extern retro_perf_register_t perf_register_cb;
 
 #define SDL_GetTicks() FAKE_SDL_TICKS
 
