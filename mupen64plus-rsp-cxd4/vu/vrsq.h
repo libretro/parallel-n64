553c1aa14dce4015dd7a6a11c375b6ee7e4d3c0c
twinaphex <libretro@gmail.com>
2014-01-01 09:02:00 +0100

    (Backport cxd4) decided to implement the VRSQ op, with a warning

--- mupen64plus-rsp-cxd4/vu/vrsq.h
+++ mupen64plus-rsp-cxd4/vu/vrsq.h
@@ -1,11 +1,14 @@
 #include "vu.h"
 #include "divrom.h"
 
-static void VRSQ(int vd, int vs, int vt, int e)
+static void VRSQ(int vd, int de, int vt, int e)
 {
-    vd &= vs &= vt &= e &= 0; /* unused */
-    if (vd == vs && vt == e)
-        message("VRSQ\nUnimplemented.", 3);
-    DPH = 0;
-    return;
+   message("VRSQ\nUntested.", 1);
+
+   DivIn = (int)VR[vt][e & 07];
+   do_div(DivIn, SP_DIV_SQRT_YES, SP_DIV_PRECISION_SINGLE);
+   SHUFFLE_VECTOR(VACC_L, VR[vt], e);
+   VR[vd][de &= 07] = (short)DivOut;
+   DPH = SP_DIV_PRECISION_SINGLE;
+   return;
 }
