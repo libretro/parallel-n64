2ecfe730294362a3f104c09ab8a446d8d5fd99c2
twinaphex <libretro@gmail.com>
2014-01-08 06:51:54 +0100

    (RSP HLE) Take out empty display list early return now that it's properly implemented in Glide64

--- mupen64plus-rsp-hle/src/main.c
+++ mupen64plus-rsp-hle/src/main.c
@@ -100,9 +100,6 @@ static void rsp_break(uint32_t setbits)
 
 static void forward_gfx_task(void)
 {
-   if (*(uint32_t *)(rspInfo.DMEM + 0xFF0) == 0x00000000)
-      return; /* Resident Evil 2 */
-
     if (rspInfo.ProcessDlistList != NULL)
     {
         rspInfo.ProcessDlistList();
