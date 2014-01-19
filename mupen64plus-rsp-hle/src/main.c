4b3d02fa1ea69f99f2bfc50f3088425a81f07650
twinaphex <libretro@gmail.com>
2014-01-19 01:40:56 +0100

    (RSP HLE) bsmiles32 backport - Fix regression introduced by commit 48e73c

--- mupen64plus-rsp-hle/src/main.c
+++ mupen64plus-rsp-hle/src/main.c
@@ -207,7 +207,7 @@ static int try_fast_task_dispatching()
 
 static void normal_task_dispatching()
 {
-    const uint32_t sum = sum_bytes(dram_u8(*dmem_u32(TASK_UCODE)), min(*dmem_u32(TASK_UCODE_SIZE), 0xf80) >> 1);
+    const uint32_t sum = sum_bytes((void*)dram_u32(*dmem_u32(TASK_UCODE)), min(*dmem_u32(TASK_UCODE_SIZE), 0xf80) >> 1);
 
     switch (sum)
     {
@@ -264,27 +264,27 @@ static void handle_unknown_task(uint32_t sum)
     
     // dump ucode_boot
     sprintf(&filename[0], "ucode_boot_%x.bin", sum);
-    dump_binary(filename, dram_u8(*dmem_u32(TASK_UCODE_BOOT)), *dmem_u32(TASK_UCODE_BOOT_SIZE));
+    dump_binary(filename, (void*)dram_u32(*dmem_u32(TASK_UCODE_BOOT)), *dmem_u32(TASK_UCODE_BOOT_SIZE));
 
     // dump ucode
     if (ucode != 0)
     {
         sprintf(&filename[0], "ucode_%x.bin", sum);
-        dump_binary(filename, dram_u8(ucode), 0xf80);
+        dump_binary(filename, (void*)dram_u32(ucode), 0xf80);
     }
 
     // dump ucode_data
     if (ucode_data != 0)
     {
         sprintf(&filename[0], "ucode_data_%x.bin", sum);
-        dump_binary(filename, dram_u8(ucode_data), *dmem_u32(TASK_UCODE_DATA_SIZE));
+        dump_binary(filename, (void*)dram_u32(ucode_data), *dmem_u32(TASK_UCODE_DATA_SIZE));
     }
 
     // dump data
     if (data_ptr != 0)
     {
         sprintf(&filename[0], "data_%x.bin", sum);
-        dump_binary(filename, dram_u8(data_ptr), *dmem_u32(TASK_DATA_SIZE));
+        dump_binary(filename, (void*)dram_u32(data_ptr), *dmem_u32(TASK_DATA_SIZE));
     }
 #endif
 }
