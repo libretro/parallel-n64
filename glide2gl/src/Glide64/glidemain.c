528987d3b8cf7db74305564da63aacab6468a4b7
twinaphex <libretro@gmail.com>
2014-02-03 05:04:16 +0100

    (Glide64) Early return of newSwapBuffers - do call to retro_return with param false

--- glide2gl/src/Glide64/glidemain.c
+++ glide2gl/src/Glide64/glidemain.c
@@ -2734,7 +2734,10 @@ void glide_set_filtering(unsigned value)
 void newSwapBuffers(void)
 {
    if (!rdp.updatescreen)
+   {
+      retro_return(false);
       return;
+   }
 
    rdp.updatescreen = 0;
 
