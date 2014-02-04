429153d38216f2eb4a739af92dbf58d1094cc6ac
twinaphex <libretro@gmail.com>
2014-02-05 00:22:04 +0100

    (Glide64) Use texrect in pm_palette_mod

--- glide2gl/src/Glide64/rdp.c
+++ glide2gl/src/Glide64/rdp.c
@@ -1143,20 +1143,24 @@ static void ys_memrect(uint32_t w0, uint32_t w1)
 
 static void pm_palette_mod(void)
 {
-   int i;
-   uint8_t envr = (uint8_t)((float)((rdp.env_color >> 24)&0xFF)/255.0f*31.0f);
-   uint8_t envg = (uint8_t)((float)((rdp.env_color >> 16)&0xFF)/255.0f*31.0f);
-   uint8_t envb = (uint8_t)((float)((rdp.env_color >> 8)&0xFF)/255.0f*31.0f);
-   uint16_t env16 = (uint16_t)((envr<<11)|(envg<<6)|(envb<<1)|1);
-   uint8_t prmr = (uint8_t)((float)((rdp.prim_color >> 24)&0xFF)/255.0f*31.0f);
-   uint8_t prmg = (uint8_t)((float)((rdp.prim_color >> 16)&0xFF)/255.0f*31.0f);
-   uint8_t prmb = (uint8_t)((float)((rdp.prim_color >> 8)&0xFF)/255.0f*31.0f);
-   uint16_t prim16 = (uint16_t)((prmr<<11)|(prmg<<6)|(prmb<<1)|1);
-   uint16_t * dst = (uint16_t*)(gfx.RDRAM+rdp.cimg);
+   uint8_t envr, envg, envb;
+   uint16_t env16, prmr, prmg, prmb, prim16, *dst;
+   int8_t i;
+
+   envr = (uint8_t)(((rdp.env_color >> 24)  & 0xFF) * 0.0039215689f * 31.0f);
+   envg = (uint8_t)(((rdp.env_color >> 16)  & 0xFF) * 0.0039215689f * 31.0f);
+   envb = (uint8_t)(((rdp.env_color >> 8 )  & 0xFF) * 0.0039215689f * 31.0f);
+   env16 = (uint16_t)((envr<<11)|(envg<<6)|(envb<<1)|1);
+   prmr = (uint8_t)(((rdp.prim_color >> 24) & 0xFF) * 0.0039215689f * 31.0f);
+   prmg = (uint8_t)(((rdp.prim_color >> 16) & 0xFF) * 0.0039215689f * 31.0f);
+   prmb = (uint8_t)(((rdp.prim_color >> 8 ) & 0xFF) * 0.0039215689f * 31.0f);
+   prim16 = (uint16_t)((prmr<<11)|(prmg<<6)|(prmb<<1)|1);
+   dst = (uint16_t*)(gfx.RDRAM+rdp.cimg);
+
    for (i = 0; i < 16; i++)
       dst[i^1] = (rdp.pal_8[i]&1) ? prim16 : env16;
 
-   LRDP("Texrect palette modification\n");
+   //LRDP("Texrect palette modification\n");
 }
 
 static void pd_zcopy(uint32_t w0, uint32_t w1)
