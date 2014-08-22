9010ff0af85bb9e10b1848cca510098fda99922a
twinaphex <libretro@gmail.com>
2014-08-22 16:55:13 +0200

    (Rice) Update CSortedList.h for BB QNX build ifx

--- gles2rice/src/CSortedList.h
+++ gles2rice/src/CSortedList.h
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef _SORTED_LIST_H_
 #define _SORTED_LIST_H_
 
-#include <cstring>
+#include <string.h>
 
 template<class Key, class Element>
 class CSortedList
