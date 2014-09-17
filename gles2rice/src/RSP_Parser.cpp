a638e1b1ca09a20529ad9d86141dfb2dcbd24479
Higor Eurípedes <heuripedes@gmail.com>
2014-09-16 23:12:03 -0300

    (Rice/MSVC) Fix another missing strncasecmp() symbol

--- gles2rice/src/RSP_Parser.cpp
+++ gles2rice/src/RSP_Parser.cpp
@@ -29,6 +29,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "ucode.h"
 #include <time.h>
 
+#ifdef _MSC_VER
+#define strncasecmp _strnicmp
+#endif
+
 
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
