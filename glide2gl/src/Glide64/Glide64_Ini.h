51f8626f257c9afa6ec035b7e878b982db8f8e46
twinaphex <libretro@gmail.com>
2014-11-03 05:35:43 +0100

    (Glide2GL) Split up customized code to Glide64_Ini.c

---
+++ glide2gl/src/Glide64/Glide64_Ini.h
@@ -0,0 +1,9 @@
+#ifndef _GLIDE64_INI_OVER_H
+#define _GLIDE64_INI_OVER_H
+
+#include "../../libretro/libretro.h"
+#include "../../libretro/SDL.h"
+
+void ReadSpecialSettings (const char * name);
+
+#endif
