7236ea33f02c60cae46c58b04e321120f687330c
twinaphex <libretro@gmail.com>
2014-11-05 17:31:29 +0100

    Add get_tex_id function to glitchtextureman.cpp

---
+++ mupen64plus-video-glide64mk2/src/Glitch64/glitchtextureman.h
@@ -0,0 +1,10 @@
+#ifndef _GLITCH_TEXTURE_MAN_H
+#define _GLITCH_TEXTURE_MAN_H
+
+void remove_tex(unsigned int idmin, unsigned int idmax);
+
+void add_tex(unsigned int id);
+
+GLuint get_tex_id(unsigned int id);
+
+#endif
