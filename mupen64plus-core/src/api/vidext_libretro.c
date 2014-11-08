4411aa62d49b0802e9c3366749b515719bf4a160
twinaphex <libretro@gmail.com>
2014-11-08 03:34:05 +0100

    Add vidext_libretro.c

---
+++ mupen64plus-core/src/api/vidext_libretro.c
@@ -0,0 +1,84 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *   Mupen64plus-core - api/vidext.c                                       *
+ *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
+ *   Copyright (C) 2009 Richard Goedeken                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       * 
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+                       
+/* This file contains the Core video extension functions which will be exported
+ * outside of the core library.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#define M64P_CORE_PROTOTYPES 1
+#include "m64p_types.h"
+#include "m64p_vidext.h"
+#include "vidext.h"
+#include "callbacks.h"
+
+/* local variables */
+static m64p_video_extension_functions l_ExternalVideoFuncTable = {10, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+static int l_VideoExtensionActive = 0;
+static int l_VideoOutputActive = 0;
+static int l_Fullscreen = 0;
+
+/* global function for use by frontend.c */
+m64p_error OverrideVideoFunctions(m64p_video_extension_functions *VideoFunctionStruct)
+{
+    /* check input data */
+    if (VideoFunctionStruct == NULL)
+        return M64ERR_INPUT_ASSERT;
+    if (VideoFunctionStruct->Functions < 11)
+        return M64ERR_INPUT_INVALID;
+
+    /* disable video extension if any of the function pointers are NULL */
+    if (VideoFunctionStruct->VidExtFuncInit == NULL ||
+        VideoFunctionStruct->VidExtFuncQuit == NULL ||
+        VideoFunctionStruct->VidExtFuncListModes == NULL ||
+        VideoFunctionStruct->VidExtFuncSetMode == NULL ||
+        VideoFunctionStruct->VidExtFuncGLGetProc == NULL ||
+        VideoFunctionStruct->VidExtFuncGLSetAttr == NULL ||
+        VideoFunctionStruct->VidExtFuncGLGetAttr == NULL ||
+        VideoFunctionStruct->VidExtFuncGLSwapBuf == NULL ||
+        VideoFunctionStruct->VidExtFuncSetCaption == NULL ||
+        VideoFunctionStruct->VidExtFuncToggleFS == NULL ||
+        VideoFunctionStruct->VidExtFuncResizeWindow == NULL)
+    {
+        l_ExternalVideoFuncTable.Functions = 11;
+        memset(&l_ExternalVideoFuncTable.VidExtFuncInit, 0, 11 * sizeof(void *));
+        l_VideoExtensionActive = 0;
+        return M64ERR_SUCCESS;
+    }
+
+    /* otherwise copy in the override function pointers */
+    memcpy(&l_ExternalVideoFuncTable, VideoFunctionStruct, sizeof(m64p_video_extension_functions));
+    l_VideoExtensionActive = 1;
+    return M64ERR_SUCCESS;
+}
+
+int VidExt_InFullscreenMode(void)
+{
+    return l_Fullscreen;
+}
+
+int VidExt_VideoRunning(void)
+{
+    return l_VideoOutputActive;
+}
+
