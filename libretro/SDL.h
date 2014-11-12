d887316472dddfb528d46ac3c2cdeedccb65aac2
twinaphex <libretro@gmail.com>
2014-11-12 16:00:22 +0100

    Add libretro_perf.h header include to libretro/SDL.h

--- libretro/SDL.h
+++ libretro/SDL.h
@@ -4,12 +4,12 @@
 #include <stdio.h>
 #include <string.h>
 #include "libretro.h"
+#include "libretro_perf.h"
 
 extern unsigned int FAKE_SDL_TICKS;
 
 extern retro_get_cpu_features_t perf_get_cpu_features_cb;
 extern retro_perf_get_counter_t perf_get_counter_cb;
-extern retro_perf_log_t perf_log_cb;
 extern retro_log_printf_t log_cb;
 extern retro_perf_register_t perf_register_cb;
 
