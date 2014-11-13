096fa4ba19325d409219a4be0e040d3ff2d15d1d
twinaphex <libretro@gmail.com>
2014-11-13 10:57:54 +0100

    (Glide64) Add HAVE_COMBINE_EXT ifdefs to TexCache.cpp

--- mupen64plus-video-glide64mk2/src/Glide64/TexCache.cpp
+++ mupen64plus-video-glide64mk2/src/Glide64/TexCache.cpp
@@ -785,7 +785,9 @@ void TexCache ()
       else
       {
         grTexCombine (tmu_1, cmb.tmu1_func, cmb.tmu1_fac, cmb.tmu1_a_func, cmb.tmu1_a_fac, cmb.tmu1_invert, cmb.tmu1_a_invert);
+#ifndef HAVE_COMBINE_EXT
         if (cmb.combine_ext)
+#endif
           cmb.grConstantColorValueExt(tmu_1, 0);
       }
       grTexDetailControl (tmu_1, cmb.dc1_lodbias, cmb.dc1_detailscale, cmb.dc1_detailmax);
@@ -813,7 +815,9 @@ void TexCache ()
       else
       {
         grTexCombine (tmu_0, cmb.tmu0_func, cmb.tmu0_fac, cmb.tmu0_a_func, cmb.tmu0_a_fac, cmb.tmu0_invert, cmb.tmu0_a_invert);
+#ifndef HAVE_COMBINE_EXT
         if (cmb.combine_ext)
+#endif
           cmb.grConstantColorValueExt(tmu_0, 0);
       }
       grTexDetailControl (tmu_0, cmb.dc0_lodbias, cmb.dc0_detailscale, cmb.dc0_detailmax);
