845ec4fd00201dec2b316a94f8337ef09421ff3f
twinaphex <libretro@gmail.com>
2015-01-12 15:08:10 +0100

    Update libco

--- libretro/libco/fiber.c
+++ libretro/libco/fiber.c
@@ -5,7 +5,7 @@
 */
 
 #define LIBCO_C
-#include "libco.h"
+#include <libco.h>
 #define WINVER 0x0400
 #define _WIN32_WINNT 0x0400
 #define WIN32_LEAN_AND_MEAN
@@ -17,33 +17,40 @@ extern "C" {
 
 static thread_local cothread_t co_active_ = 0;
 
-static void __stdcall co_thunk(void *coentry) {
-  ((void (*)(void))coentry)();
+static void __stdcall co_thunk(void *coentry)
+{
+   ((void (*)(void))coentry)();
 }
 
-cothread_t co_active() {
-  if(!co_active_) {
-    ConvertThreadToFiber(0);
-    co_active_ = GetCurrentFiber();
-  }
-  return co_active_;
+cothread_t co_active(void)
+{
+   if(!co_active_)
+   {
+      ConvertThreadToFiber(0);
+      co_active_ = GetCurrentFiber();
+   }
+   return co_active_;
 }
 
-cothread_t co_create(unsigned int heapsize, void (*coentry)(void)) {
-  if(!co_active_) {
-    ConvertThreadToFiber(0);
-    co_active_ = GetCurrentFiber();
-  }
-  return (cothread_t)CreateFiber(heapsize, co_thunk, (void*)coentry);
+cothread_t co_create(unsigned int heapsize, void (*coentry)(void))
+{
+   if(!co_active_)
+   {
+      ConvertThreadToFiber(0);
+      co_active_ = GetCurrentFiber();
+   }
+   return (cothread_t)CreateFiber(heapsize, co_thunk, (void*)coentry);
 }
 
-void co_delete(cothread_t cothread) {
-  DeleteFiber(cothread);
+void co_delete(cothread_t cothread)
+{
+   DeleteFiber(cothread);
 }
 
-void co_switch(cothread_t cothread) {
-  co_active_ = cothread;
-  SwitchToFiber(cothread);
+void co_switch(cothread_t cothread)
+{
+   co_active_ = cothread;
+   SwitchToFiber(cothread);
 }
 
 #ifdef __cplusplus
