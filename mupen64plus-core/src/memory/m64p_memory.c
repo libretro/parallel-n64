9854cb39733342c8f1eb30fdff9e2b51492519c8
twinaphex <libretro@gmail.com>
2015-01-18 01:14:20 +0100

    Backport bsmiles32 - https://github.com/mupen64plus/mupen64plus-core/commit/a8531ef3185ec2ef2e607b69a0133dd7f57bec1f

--- mupen64plus-core/src/memory/m64p_memory.c
+++ mupen64plus-core/src/memory/m64p_memory.c
@@ -1555,6 +1555,15 @@ static void update_DPC(void)
       dpc_register.dpc_status |= 0x4;
 }
 
+
+static void invalidate_code(uint32_t address)
+{
+   if (r4300emu != CORE_PURE_INTERPRETER && !invalid_code[address>>12])
+      if (blocks[address>>12]->block[(address&0xFFF)/4].ops !=
+            current_instruction_table.NOTCOMPILED)
+         invalid_code[address>>12] = 1;
+}
+
 static void pre_framebuffer_read(uint32_t address)
 {
    int i;
@@ -1660,10 +1669,7 @@ void read_nomemd(void)
 
 void write_nomem(void)
 {
-   if (r4300emu != CORE_PURE_INTERPRETER && !invalid_code[address>>12])
-      if (blocks[address>>12]->block[(address&0xFFF)/4].ops !=
-            current_instruction_table.NOTCOMPILED)
-         invalid_code[address>>12] = 1;
+   invalidate_code(address);
    address = virtual_to_physical_address(address,1);
    if (address == 0x00000000) return;
    write_word_in_memory();
@@ -1671,10 +1677,7 @@ void write_nomem(void)
 
 void write_nomemb(void)
 {
-   if (r4300emu != CORE_PURE_INTERPRETER && !invalid_code[address>>12])
-      if (blocks[address>>12]->block[(address&0xFFF)/4].ops != 
-            current_instruction_table.NOTCOMPILED)
-         invalid_code[address>>12] = 1;
+   invalidate_code(address);
    address = virtual_to_physical_address(address,1);
    if (address == 0x00000000) return;
    write_byte_in_memory();
@@ -1682,10 +1685,7 @@ void write_nomemb(void)
 
 void write_nomemh(void)
 {
-   if (r4300emu != CORE_PURE_INTERPRETER && !invalid_code[address>>12])
-      if (blocks[address>>12]->block[(address&0xFFF)/4].ops != 
-            current_instruction_table.NOTCOMPILED)
-         invalid_code[address>>12] = 1;
+   invalidate_code(address);
    address = virtual_to_physical_address(address,1);
    if (address == 0x00000000) return;
    write_hword_in_memory();
@@ -1693,10 +1693,7 @@ void write_nomemh(void)
 
 void write_nomemd(void)
 {
-   if (r4300emu != CORE_PURE_INTERPRETER && !invalid_code[address>>12])
-      if (blocks[address>>12]->block[(address&0xFFF)/4].ops != 
-            current_instruction_table.NOTCOMPILED)
-         invalid_code[address>>12] = 1;
+   invalidate_code(address);
    address = virtual_to_physical_address(address,1);
    if (address == 0x00000000) return;
    write_dword_in_memory();
