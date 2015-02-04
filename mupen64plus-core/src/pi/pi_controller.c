1f53696eac73257f135ac5bc7c06bd4a57f78711
twinaphex <libretro@gmail.com>
2015-02-04 18:36:47 +0100

    Backport bsmiles32 cenification - Move dma_pi_{read,write} into the pi_controller module. https://github.com/bsmiles32/mupen64plus-core/commit/7521e198a200deea1587886c738b3998deff2084

--- mupen64plus-core/src/pi/pi_controller.c
+++ mupen64plus-core/src/pi/pi_controller.c
@@ -21,13 +21,185 @@
 
 #include "pi_controller.h"
 
-/* XXX: move dma_pi_{read,write} here instead */
-#include "../memory/dma.h"
+#define M64P_CORE_PROTOTYPES 1
+#include "../api/m64p_types.h"
+#include "../api/m64p_config.h"
+#include "../api/callbacks.h"
+#include "../api/config.h"
+#include "../main/main.h"
+#include "../main/rom.h"
 #include "../memory/memory.h"
+#include "../r4300/cached_interp.h"
+#include "../r4300/cp0.h"
 #include "../r4300/interupt.h"
+#include "../r4300/r4300.h"
 #include "../r4300/r4300_core.h"
+#include "../ri/ri_controller.h"
 
 #include <string.h>
+
+int32_t enable_expmem = 1;
+
+void dma_enable_expmem(int enable)
+{
+   enable_expmem = enable;
+}
+
+static void dma_pi_read(struct pi_controller *pi)
+{
+   if (pi->regs[PI_CART_ADDR_REG] >= 0x08000000
+         && pi->regs[PI_CART_ADDR_REG] < 0x08010000)
+   {
+      if (pi->use_flashram != 1)
+      {
+         dma_write_sram(pi);
+         pi->use_flashram = -1;
+      }
+      else
+      {
+         dma_write_flashram(pi);
+      }
+   }
+   else
+   {
+      DebugMessage(M64MSG_WARNING, "Unknown dma read in dma_pi_read()");
+   }
+
+   pi->regs[PI_STATUS_REG] |= 1;
+   update_count();
+   add_interupt_event(PI_INT, 0x1000/* pi->regs[PI_RD_LEN_REG] */);
+}
+
+static void dma_pi_write(struct pi_controller *pi)
+{
+   uint32_t longueur;
+   int32_t i;
+
+   if (pi->regs[PI_CART_ADDR_REG] < 0x10000000)
+   {
+      if (pi->regs[PI_CART_ADDR_REG] >= 0x08000000
+            && pi->regs[PI_CART_ADDR_REG] < 0x08010000)
+      {
+         if (pi->use_flashram != 1)
+         {
+            dma_read_sram(pi);
+            pi->use_flashram = -1;
+         }
+         else
+         {
+            dma_read_flashram(pi);
+         }
+      }
+      else if (pi->regs[PI_CART_ADDR_REG] >= 0x06000000
+            && pi->regs[PI_CART_ADDR_REG] < 0x08000000)
+      {
+      }
+      else
+      {
+         DebugMessage(M64MSG_WARNING, "Unknown dma write 0x%x in dma_pi_write()", (int32_t)pi->regs[PI_CART_ADDR_REG]);
+      }
+
+      pi->regs[PI_STATUS_REG] |= 1;
+      update_count();
+      add_interupt_event(PI_INT, /* pi->regs[PI_WR_LEN_REG]*/0x1000);
+
+      return;
+   }
+
+   if (pi->regs[PI_CART_ADDR_REG] >= 0x1fc00000) // for paper mario
+   {
+      pi->regs[PI_STATUS_REG] |= 1;
+      update_count();
+      add_interupt_event(PI_INT, 0x1000);
+
+      return;
+   }
+
+   longueur = (pi->regs[PI_WR_LEN_REG] & 0xFFFFFF)+1;
+   i = (pi->regs[PI_CART_ADDR_REG]-0x10000000)&0x3FFFFFF;
+   longueur = (i + (int32_t)longueur) > g_rom_size ?
+      (g_rom_size - i) : longueur;
+   longueur = (pi->regs[PI_DRAM_ADDR_REG] + longueur) > 0x7FFFFF ?
+      (0x7FFFFF - pi->regs[PI_DRAM_ADDR_REG]) : longueur;
+
+   if (i>g_rom_size || pi->regs[PI_DRAM_ADDR_REG] > 0x7FFFFF)
+   {
+      pi->regs[PI_STATUS_REG] |= 3;
+      update_count();
+      add_interupt_event(PI_INT, longueur/8);
+
+      return;
+   }
+
+   if (r4300emu != CORE_PURE_INTERPRETER)
+   {
+      for (i=0; i<(int32_t)longueur; i++)
+      {
+         uint32_t rdram_address1 = pi->regs[PI_DRAM_ADDR_REG]+i+0x80000000;
+         uint32_t rdram_address2 = pi->regs[PI_DRAM_ADDR_REG]+i+0xa0000000;
+         ((uint8_t*)g_rdram)[(pi->regs[PI_DRAM_ADDR_REG]+i)^S8]=
+            g_rom[(((pi->regs[PI_CART_ADDR_REG]-0x10000000)&0x3FFFFFF)+i)^S8];
+
+         if (!invalid_code[rdram_address1>>12])
+         {
+            if (!blocks[rdram_address1>>12] ||
+                  blocks[rdram_address1>>12]->block[(rdram_address1&0xFFF)/4].ops !=
+                  current_instruction_table.NOTCOMPILED)
+            {
+               invalid_code[rdram_address1>>12] = 1;
+            }
+#ifdef NEW_DYNAREC
+            invalidate_block(rdram_address1>>12);
+#endif
+         }
+         if (!invalid_code[rdram_address2>>12])
+         {
+            if (!blocks[rdram_address1>>12] ||
+                  blocks[rdram_address2>>12]->block[(rdram_address2&0xFFF)/4].ops !=
+                  current_instruction_table.NOTCOMPILED)
+            {
+               invalid_code[rdram_address2>>12] = 1;
+            }
+         }
+      }
+   }
+   else
+   {
+      for (i=0; i<(int32_t)longueur; i++)
+      {
+         ((uint8_t*)g_rdram)[(pi->regs[PI_DRAM_ADDR_REG]+i)^S8]=
+            g_rom[(((pi->regs[PI_CART_ADDR_REG]-0x10000000)&0x3FFFFFF)+i)^S8];
+      }
+   }
+
+   // Set the RDRAM memory size when copying main ROM code
+   // (This is just a convenient way to run this code once at the beginning)
+   if (pi->regs[PI_CART_ADDR_REG] == 0x10001000)
+   {
+      switch (g_cic_type)
+      {
+         case CIC_X101:
+         case CIC_X102:
+         case CIC_X103:
+         case CIC_X106:
+            if (enable_expmem)
+               g_rdram[0x318/4] = 0x800000;
+            else
+               g_rdram[0x318/4] = 0x400000;
+            break;
+         case CIC_X105:
+            if (enable_expmem)
+               g_rdram[0x3F0/4] = 0x800000;
+            else
+               g_rdram[0x3F0/4] = 0x400000;
+            break;
+      }
+   }
+
+   pi->regs[PI_STATUS_REG] |= 3;
+   update_count();
+   add_interupt_event(PI_INT, longueur/8);
+}
 
 void connect_pi(struct pi_controller* pi,
                 struct r4300_core* r4300,
@@ -68,12 +240,12 @@ int write_pi_regs(void* opaque, uint32_t address, uint32_t value, uint32_t mask)
     {
     case PI_RD_LEN_REG:
         masked_write(&pi->regs[PI_RD_LEN_REG], value, mask);
-        dma_pi_read();
+        dma_pi_read(pi);
         return 0;
 
     case PI_WR_LEN_REG:
         masked_write(&pi->regs[PI_WR_LEN_REG], value, mask);
-        dma_pi_write();
+        dma_pi_write(pi);
         return 0;
 
     case PI_STATUS_REG:
