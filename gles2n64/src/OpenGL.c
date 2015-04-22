ce52f9b2d0bc6edf905fbf9fc6f7b14c30442b89
twinaphex <libretro@gmail.com>
2015-04-22 15:34:45 +0200

    (GLN64) Backport OGL_SetBlendMode from GlideN64

--- gles2n64/src/OpenGL.c
+++ gles2n64/src/OpenGL.c
@@ -187,135 +187,122 @@ void OGL_UpdateScissor(void)
 //copied from RICE VIDEO
 void OGL_SetBlendMode(void)
 {
+	const u32 blendmode = gDP.otherMode.l >> 16;
+	// 0x7000 = CVG_X_ALPHA|ALPHA_CVG_SEL|FORCE_BL
+	if (gDP.otherMode.alphaCvgSel != 0 && (gDP.otherMode.l & 0x7000) != 0x7000) {
+		switch (blendmode) {
+		case 0x4055: // Mario Golf
+		case 0x5055: // Paper Mario intro clr_mem * a_in + clr_mem * a_mem
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_ZERO, GL_ONE);
+			break;
+		default:
+			glDisable(GL_BLEND);
+		}
+		return;
+	}
 
-   u32 blender = gDP.otherMode.l >> 16;
-   u32 blendmode_1 = blender&0xcccc;
-   u32 blendmode_2 = blender&0x3333;
+	if (gDP.otherMode.forceBlender != 0 && gDP.otherMode.cycleType < G_CYC_COPY) {
+		glEnable( GL_BLEND );
 
-   glEnable(GL_BLEND);
-   switch(gDP.otherMode.cycleType)
-   {
-      case G_CYC_FILL:
-         glDisable(GL_BLEND);
-         break;
+		switch (blendmode)
+		{
+			// Mace objects
+			case 0x0382:
+			// Mace special blend mode, see GLSLCombiner.cpp
+			case 0x0091:
+			// 1080 Sky
+			case 0x0C08:
+			// Used LOTS of places
+			case 0x0F0A:
+			//DK64 blue prints
+			case 0x0302:
+			// Bomberman 2 special blend mode, see GLSLCombiner.cpp
+			case 0xA500:
+			//Sin and Punishment
+			case 0xCB02:
+			// Battlezone
+			// clr_in * a + clr_in * (1-a)
+			case 0xC800:
+			// Conker BFD
+			// clr_in * a_fog + clr_fog * (1-a)
+			// clr_in * 0 + clr_in * 1
+			case 0x07C2:
+			case 0x00C0:
+			//ISS64
+			case 0xC302:
+			// Donald Duck
+			case 0xC702:
+				glBlendFunc(GL_ONE, GL_ZERO);
+				break;
 
-      case G_CYC_COPY:
-         glBlendFunc(GL_ONE, GL_ZERO);
-         break;
+			case 0x0F1A:
+				if (gDP.otherMode.cycleType == G_CYC_1CYCLE)
+					glBlendFunc(GL_ONE, GL_ZERO);
+				else
+					glBlendFunc(GL_ZERO, GL_ONE);
+				break;
 
-      case G_CYC_2CYCLE:
-         if (gDP.otherMode.forceBlender && gDP.otherMode.depthCompare)
-         {
-            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-            break;
-         }
+			//Space Invaders
+			case 0x0448: // Add
+			case 0x055A:
+				glBlendFunc( GL_ONE, GL_ONE );
+				break;
 
-         switch(blendmode_1+blendmode_2)
-         {
-            case BLEND_PASS+(BLEND_PASS>>2):    // In * 0 + In * 1
-            case BLEND_FOG_APRIM+(BLEND_PASS>>2):
-            case BLEND_FOG_MEM_FOG_MEM + (BLEND_OPA>>2):
-            case BLEND_FOG_APRIM + (BLEND_OPA>>2):
-            case BLEND_FOG_ASHADE + (BLEND_OPA>>2):
-            case BLEND_BI_AFOG + (BLEND_OPA>>2):
-            case BLEND_FOG_ASHADE + (BLEND_NOOP>>2):
-            case BLEND_NOOP + (BLEND_OPA>>2):
-            case BLEND_NOOP4 + (BLEND_NOOP>>2):
-            case BLEND_FOG_ASHADE+(BLEND_PASS>>2):
-            case BLEND_FOG_3+(BLEND_PASS>>2):
-               glDisable(GL_BLEND);
-               break;
+			case 0xc712: // Pokemon Stadium?
+			case 0xAF50: // LOT in Zelda: MM
+			case 0x0F5A: // LOT in Zelda: MM
+			case 0x0FA5: // Seems to be doing just blend color - maybe combiner can be used for this?
+			case 0x5055: // Used in Paper Mario intro, I'm not sure if this is right...
+				//clr_in * 0 + clr_mem * 1
+				glBlendFunc( GL_ZERO, GL_ONE );
+				break;
 
-            case BLEND_PASS+(BLEND_OPA>>2):
-               if (gDP.otherMode.cvgXAlpha && gDP.otherMode.alphaCvgSel)
-               glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-               else
-                  glDisable(GL_BLEND);
-               break;
+			case 0x5F50: //clr_mem * 0 + clr_mem * (1-a)
+				glBlendFunc( GL_ZERO, GL_ONE_MINUS_SRC_ALPHA );
+				break;
 
-            case BLEND_PASS + (BLEND_XLU>>2):
-            case BLEND_FOG_ASHADE + (BLEND_XLU>>2):
-            case BLEND_FOG_APRIM + (BLEND_XLU>>2):
-            case BLEND_FOG_MEM_FOG_MEM + (BLEND_PASS>>2):
-            case BLEND_XLU + (BLEND_XLU>>2):
-            case BLEND_BI_AFOG + (BLEND_XLU>>2):
-            case BLEND_XLU + (BLEND_FOG_MEM_IN_MEM>>2):
-            case BLEND_PASS + (BLEND_FOG_MEM_IN_MEM>>2):
-               glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-               break;
+			case 0xF550: //clr_fog * a_fog + clr_mem * (1-a)
+			case 0x0150: // spiderman
+			case 0x0550: // bomberman 64
+			case 0x0D18: //clr_in * a_fog + clr_mem * (1-a)
+				glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+				break;
 
-            case BLEND_FOG_ASHADE+0x0301:
-               glBlendFunc(GL_SRC_ALPHA, GL_ZERO);
-               break;
+			case 0xC912: //40 winks, clr_in * a_fog + clr_mem * 1
+				glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+				break;
 
-            case 0x0c08+0x1111:
-               glBlendFunc(GL_ZERO, GL_DST_ALPHA);
-               break;
+			case 0x0040: // Fzero
+			case 0xC810: // Blends fog
+			case 0xC811: // Blends fog
+			case 0x0C18: // Standard interpolated blend
+			case 0x0C19: // Used for antialiasing
+			case 0x0050: // Standard interpolated blend
+			case 0x0051: // Standard interpolated blend
+			case 0x0055: // Used for antialiasing
+				glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+				break;
 
-            default:
-               if (blendmode_2 == (BLEND_PASS>>2))
-                  glDisable(GL_BLEND);
-               else
-                  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-               break;
-         }
-         break;
 
-      default:
-
-         if (gDP.otherMode.forceBlender && gDP.otherMode.depthCompare && blendmode_1 != BLEND_FOG_ASHADE )
-         {
-            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-            break;
-         }
-
-         switch (blendmode_1)
-         {
-            case BLEND_XLU:
-            case BLEND_BI_AIN:
-            case BLEND_FOG_MEM:
-            case BLEND_FOG_MEM_IN_MEM:
-            case BLEND_BLENDCOLOR:
-            case 0x00c0:
-               glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-               break;
-
-            case BLEND_MEM_ALPHA_IN:
-               glBlendFunc(GL_ZERO, GL_DST_ALPHA);
-               break;
-
-            case BLEND_OPA:
-               //if( options.enableHackForGames == HACK_FOR_MARIO_TENNIS )
-               //{
-               //   glBlendFunc(BLEND_SRCALPHA, BLEND_INVSRCALPHA);
-               //}
-
-               glDisable(GL_BLEND);
-               break;
-
-            case BLEND_PASS:
-            case BLEND_NOOP:
-            case BLEND_FOG_ASHADE:
-            case BLEND_FOG_MEM_3:
-            case BLEND_BI_AFOG:
-               glDisable(GL_BLEND);
-               break;
-
-            case BLEND_FOG_APRIM:
-               glBlendFunc(GL_ONE_MINUS_SRC_ALPHA, GL_ZERO);
-               break;
-
-            case BLEND_NOOP3:
-            case BLEND_NOOP5:
-            case BLEND_MEM:
-               glBlendFunc(GL_ZERO, GL_ONE);
-               break;
-
-            default:
-               glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-         }
-   }
-
+			default:
+				LOG(LOG_VERBOSE, "Unhandled blend mode=%x", gDP.otherMode.l >> 16);
+				glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+				break;
+		}
+	}
+#if 0
+   else if ((config.generalEmulation.hacks & hack_pilotWings) != 0 && (gDP.otherMode.l & 0x80) != 0) { //CLR_ON_CVG without FORCE_BL
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_ZERO, GL_ONE);
+	} else if ((config.generalEmulation.hacks & hack_blastCorps) != 0 && gSP.texture.on == 0 && currentCombiner()->usesTex()) { // Blast Corps
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_ZERO, GL_ONE);
+	}
+#endif
+   else {
+		glDisable( GL_BLEND );
+	}
 }
 
 void OGL_UpdateStates(void)
