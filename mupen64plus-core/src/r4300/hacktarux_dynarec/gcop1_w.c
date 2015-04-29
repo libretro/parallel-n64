1aaf04c9a86e0554d01042f2d88e129372493b70
twinaphex <libretro@gmail.com>
2015-04-30 00:35:26 +0200

    Move gcop1_w.c to Hacktarux

--- mupen64plus-core/src/r4300/hacktarux_dynarec/gcop1_w.c
+++ mupen64plus-core/src/r4300/hacktarux_dynarec/gcop1_w.c
@@ -1,6 +1,7 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *   Mupen64plus - gcop1_w.c                                               *
  *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
+ *   Copyright (C) 2007 Richard Goedeken (Richard42)                       *
  *   Copyright (C) 2002 Hacktarux                                          *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -21,8 +22,17 @@
 
 #include <stdio.h>
 
-#include "assemble.h"
-#include "interpret.h"
+#ifdef __x86_64__
+#include "../x86_64/assemble.h"
+#include "../x86_64/interpret.h"
+
+typedef uint64_t native_type;
+#else
+#include "../x86/assemble.h"
+#include "../x86/interpret.h"
+
+typedef uint32_t native_type;
+#endif
 
 #include "r4300/recomph.h"
 #include "r4300/r4300.h"
@@ -32,26 +42,40 @@
 void gencvt_s_w(void)
 {
 #ifdef INTERPRET_CVT_S_W
-   gencallinterp((unsigned int)cached_interpreter_table.CVT_S_W, 0);
+   gencallinterp((native_type)cached_interpreter_table.CVT_S_W, 0);
 #else
    gencheck_cop1_unusable();
+#ifdef __x86_64__
+   mov_xreg64_m64rel(RAX, (unsigned long long *)(&reg_cop1_simple[dst->f.cf.fs]));
+   fild_preg64_dword(RAX);
+   mov_xreg64_m64rel(RAX, (unsigned long long *)(&reg_cop1_simple[dst->f.cf.fd]));
+   fstp_preg64_dword(RAX);
+#else
    mov_eax_memoffs32((unsigned int*)(&reg_cop1_simple[dst->f.cf.fs]));
    fild_preg32_dword(EAX);
    mov_eax_memoffs32((unsigned int*)(&reg_cop1_simple[dst->f.cf.fd]));
    fstp_preg32_dword(EAX);
+#endif
 #endif
 }
 
 void gencvt_d_w(void)
 {
 #ifdef INTERPRET_CVT_D_W
-   gencallinterp((unsigned int)cached_interpreter_table.CVT_D_W, 0);
+   gencallinterp((native_type)cached_interpreter_table.CVT_D_W, 0);
 #else
    gencheck_cop1_unusable();
+#ifdef __x86_64__
+   mov_xreg64_m64rel(RAX, (unsigned long long *)(&reg_cop1_simple[dst->f.cf.fs]));
+   fild_preg64_dword(RAX);
+   mov_xreg64_m64rel(RAX, (unsigned long long *)(&reg_cop1_double[dst->f.cf.fd]));
+   fstp_preg64_qword(RAX);
+#else
    mov_eax_memoffs32((unsigned int*)(&reg_cop1_simple[dst->f.cf.fs]));
    fild_preg32_dword(EAX);
    mov_eax_memoffs32((unsigned int*)(&reg_cop1_double[dst->f.cf.fd]));
    fstp_preg32_qword(EAX);
+#endif
 #endif
 }
 
