e524b94571ab4f46ae12c79549b2b71b715feab8
twinaphex <libretro@gmail.com>
2015-05-05 01:26:52 +0200

    (Use gdp_set_tile_size in load_block

--- mupen64plus-video-angrylion/n64video_rdp.c
+++ mupen64plus-video-angrylion/n64video_rdp.c
@@ -851,19 +851,15 @@ static void set_tile_size(void)
 static void load_block(void)
 {
     INT32 lewdata[10];
-    const int command = (cmd_data[cmd_cur + 0].UW32[0] & 0xFF000000) >> (56-32);
-    const int sl      = (cmd_data[cmd_cur + 0].UW32[0] & 0x00FFF000) >> (44-32);
-    const int tl      = (cmd_data[cmd_cur + 0].UW32[0] & 0x00000FFF) >> (32-32);
-    const int tilenum = (cmd_data[cmd_cur + 0].UW32[1] & 0x07000000) >> (24- 0);
-    const int sh      = (cmd_data[cmd_cur + 0].UW32[1] & 0x00FFF000) >> (12- 0);
-    const int dxt     = (cmd_data[cmd_cur + 0].UW32[1] & 0x00000FFF) >> ( 0- 0);
+    uint32_t w0 = cmd_data[cmd_cur + 0].UW32[0];
+    uint32_t w1 = cmd_data[cmd_cur + 0].UW32[1];
+    const int command = (w0 & 0xFF000000) >> (56-32);
+    const int sl      = (w0 & 0x00FFF000) >> (44-32);
+    const int tl      = (w0 & 0x00000FFF) >> (32-32);
+    const int sh      = (w1 & 0x00FFF000) >> (12- 0);
+    const int dxt     = (w1 & 0x00000FFF) >> ( 0- 0);
     const int tlclamped = tl & 0x3FF;
-
-    g_gdp.tile[tilenum].sl = sl;
-    g_gdp.tile[tilenum].tl = tl;
-    g_gdp.tile[tilenum].sh = sh;
-    g_gdp.tile[tilenum].th = dxt;
-
+    int32_t tilenum     = gdp_set_tile_size(w0, w1);
     calculate_clamp_diffs(tilenum);
 
     lewdata[0] =
