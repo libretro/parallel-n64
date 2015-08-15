1ec2a3a39040b6c732b9bc94803be81d6e33da83
twinaphex <libretro@gmail.com>
2015-08-15 23:13:27 +0200

    Revert "Rewrite dma_sp_read/dma_sp_write into one common function"
    This reverts commit 262e09536411f4223455728d585d651b661f82c8.

--- mupen64plus-core/src/rsp/rsp_core.c
+++ mupen64plus-core/src/rsp/rsp_core.c
@@ -32,10 +32,11 @@
 #include <stdio.h>
 #include <string.h>
 
-static void dma_sp_do(struct rsp_core* sp, unsigned memaddr, unsigned dramaddr,
-      unsigned length, unsigned count, unsigned skip)
+static void dma_sp_write(struct rsp_core* sp, unsigned length, unsigned count, unsigned skip)
 {
     unsigned int i,j;
+    unsigned int memaddr  = sp->regs[SP_MEM_ADDR_REG] & 0xfff;
+    unsigned int dramaddr = sp->regs[SP_DRAM_ADDR_REG] & 0xffffff;
 
     unsigned char *spmem  = (unsigned char*)sp->mem + (sp->regs[SP_MEM_ADDR_REG] & 0x1000);
     unsigned char *dram   = (unsigned char*)sp->ri->rdram.dram;
@@ -43,8 +44,32 @@ static void dma_sp_do(struct rsp_core* sp, unsigned memaddr, unsigned dramaddr,
     for(j = 0; j < count; j++)
     {
         for(i = 0; i < length; i++)
-            spmem[memaddr++^S8] = dram[dramaddr++^S8];
+        {
+            spmem[memaddr^S8] = dram[dramaddr^S8];
+            memaddr++;
+            dramaddr++;
+        }
+        dramaddr+=skip;
+    }
+}
 
+static void dma_sp_read(struct rsp_core* sp, unsigned length, unsigned count, unsigned skip)
+{
+    unsigned int i,j;
+    unsigned int memaddr  = sp->regs[SP_MEM_ADDR_REG] & 0xfff;
+    unsigned int dramaddr = sp->regs[SP_DRAM_ADDR_REG] & 0xffffff;
+
+    unsigned char *spmem  = (unsigned char*)sp->mem + (sp->regs[SP_MEM_ADDR_REG] & 0x1000);
+    unsigned char *dram   = (unsigned char*)sp->ri->rdram.dram;
+
+    for(j = 0; j < count; j++)
+    {
+        for(i = 0; i < length; i++)
+        {
+            dram[dramaddr^S8] = spmem[memaddr^S8];
+            memaddr++;
+            dramaddr++;
+        }
         dramaddr+=skip;
     }
 }
@@ -170,7 +195,6 @@ int read_rsp_regs(void* opaque, uint32_t address, uint32_t* value)
 
 int write_rsp_regs(void* opaque, uint32_t address, uint32_t value, uint32_t mask)
 {
-   unsigned memaddr, dramaddr;
    unsigned l, length, count, skip;
    struct rsp_core* sp = (struct rsp_core*)opaque;
    uint32_t reg        = RSP_REG(address);
@@ -193,19 +217,15 @@ int write_rsp_regs(void* opaque, uint32_t address, uint32_t value, uint32_t mask
           length   = ((l & 0xfff) | 7) + 1;
           count    = ((l >> 12) & 0xff) + 1;
           skip     = ((l >> 20) & 0xfff);
-          memaddr  = sp->regs[SP_MEM_ADDR_REG] & 0xfff;
-          dramaddr = sp->regs[SP_DRAM_ADDR_REG] & 0xffffff;
 
-          dma_sp_do(sp, memaddr, dramaddr, length, count, skip);
+          dma_sp_write(sp, length, count, skip);
           break;
        case SP_WR_LEN_REG:
           l        = sp->regs[SP_WR_LEN_REG];
           length   = ((l & 0xfff)) + 1;
           count    = ((l >> 12) & 0xff) + 1;
           skip     = ((l >> 20) & 0xfff);
-          memaddr  = sp->regs[SP_MEM_ADDR_REG] & 0xfff;
-          dramaddr = sp->regs[SP_DRAM_ADDR_REG] & 0xffffff;
-          dma_sp_do(sp, dramaddr, memaddr, length, count, skip);
+          dma_sp_read(sp, length, count, skip);
           break;
        case SP_SEMAPHORE_REG:
           sp->regs[SP_SEMAPHORE_REG] = 0;
