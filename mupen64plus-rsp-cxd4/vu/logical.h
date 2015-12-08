ef2360800a8c0724ccf443944f770fb9af735604
twinaphex <libretro@gmail.com>
2015-12-08 20:41:56 +0100

    Combine more headers

--- mupen64plus-rsp-cxd4/vu/logical.h
+++ mupen64plus-rsp-cxd4/vu/logical.h
@@ -13,6 +13,15 @@
 \******************************************************************************/
 #include "vu.h"
 
+static INLINE void do_and(short* VD, short* VS, short* VT)
+{
+   register int i;
+
+   for (i = 0; i < N; i++)
+      VACC_L[i] = VS[i] & VT[i];
+   vector_copy(VD, VACC_L);
+}
+
 static INLINE void do_nand(short* VD, short* VS, short* VT)
 {
    register int i;
@@ -20,6 +29,65 @@ static INLINE void do_nand(short* VD, short* VS, short* VT)
    for (i = 0; i < N; i++)
       VACC_L[i] = ~(VS[i] & VT[i]);
    vector_copy(VD, VACC_L);
+}
+
+static INLINE void do_nor(short* VD, short* VS, short* VT)
+{
+   register int i;
+
+   for (i = 0; i < N; i++)
+      VACC_L[i] = ~(VS[i] | VT[i]);
+   vector_copy(VD, VACC_L);
+}
+
+static INLINE void do_nxor(short* VD, short* VS, short* VT)
+{
+   register int i;
+
+   for (i = 0; i < N; i++)
+      VACC_L[i] = ~(VS[i] ^ VT[i]);
+   vector_copy(VD, VACC_L);
+}
+
+static INLINE void VNXOR(int vd, int vs, int vt, int e)
+{
+   short ST[N];
+
+   SHUFFLE_VECTOR(ST, VR[vt], e);
+   do_nxor(VR[vd], VR[vs], ST);
+}
+
+static INLINE void VNOR(int vd, int vs, int vt, int e)
+{
+   short ST[N];
+
+   SHUFFLE_VECTOR(ST, VR[vt], e);
+   do_nor(VR[vd], VR[vs], ST);
+}
+
+static INLINE void do_or(short* VD, short* VS, short* VT)
+{
+    register int i;
+
+    for (i = 0; i < N; i++)
+        VACC_L[i] = VS[i] | VT[i];
+    vector_copy(VD, VACC_L);
+}
+
+static INLINE void VOR(int vd, int vs, int vt, int e)
+{
+   short ST[N];
+
+   SHUFFLE_VECTOR(ST, VR[vt], e);
+   do_or(VR[vd], VR[vs], ST);
+}
+
+static INLINE void VAND(int vd, int vs, int vt, int e)
+{
+   short ST[N];
+
+   SHUFFLE_VECTOR(ST, VR[vt], e);
+   do_and(VR[vd], VR[vs], ST);
 }
 
 static INLINE void VNAND(int vd, int vs, int vt, int e)
