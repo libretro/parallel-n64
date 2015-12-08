b1a46fbc4bc89898a4bfcca79750d0418dcb1028
twinaphex <libretro@gmail.com>
2015-12-08 19:35:30 +0100

    Start grouping headers together

--- mupen64plus-rsp-cxd4/vu/vmacf.h
+++ mupen64plus-rsp-cxd4/vu/vmacf.h
@@ -39,6 +39,45 @@ INLINE static void do_macf(short* VD, short* VS, short* VT)
         VACC_H[i] += addend[i] >> 16;
 }
 
+#ifndef SEMIFRAC
+/*
+ * acc = VS * VT;
+ * acc = acc + 0x8000; // rounding value
+ * acc = acc << 1; // partial value shifting
+ *
+ * Wrong:  ACC(HI) = -((INT32)(acc) < 0)
+ * Right:  ACC(HI) = -(SEMIFRAC < 0)
+ */
+#define SEMIFRAC    (VS[i]*VT[i]*2/2 + 0x8000/2)
+#endif
+
+INLINE static void do_mulf(short* VD, short* VS, short* VT)
+{
+    register int i;
+
+    for (i = 0; i < N; i++)
+        VACC_L[i] = (SEMIFRAC << 1) >>  0;
+    for (i = 0; i < N; i++)
+        VACC_M[i] = (SEMIFRAC << 1) >> 16;
+    for (i = 0; i < N; i++)
+        VACC_H[i] = -((VACC_M[i] < 0) & (VS[i] != VT[i])); /* -32768 * -32768 */
+#ifndef ARCH_MIN_SSE2
+    vector_copy(VD, VACC_M);
+    for (i = 0; i < N; i++)
+        VD[i] -= (VACC_M[i] < 0) & (VS[i] == VT[i]); /* ACC b 31 set, min*min */
+#else
+    SIGNED_CLAMP_AM(VD);
+#endif
+}
+
+static void VMULF(int vd, int vs, int vt, int e)
+{
+    short ST[N];
+
+    SHUFFLE_VECTOR(ST, VR[vt], e);
+    do_mulf(VR[vd], VR[vs], ST);
+}
+
 static void VMACF(int vd, int vs, int vt, int e)
 {
    short ST[N];
