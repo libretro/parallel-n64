33559f19a4483fe866b9fbf7984a0f6949ac6711
twinaphex <libretro@gmail.com>
2016-01-30 12:35:10 +0100

    Revert "(GLN64) Backport https://github.com/gonetz/GLideN64/commit/e8d8ee93e45e2304d09b312f7bf2eea8b3a1013f"
    This reverts commit b65eb38b240e2d26298c8b8e33421797e6225012.

--- gles2n64/src/RSP.h
+++ gles2n64/src/RSP.h
@@ -31,7 +31,7 @@ extern RSPInfo __RSP;
 
 extern u32 DepthClearColor;
 
-#define RSP_SegmentToPhysical( segaddr ) ((gSP.segment[(segaddr >> 24) & 0x0F] + (segaddr & RDRAMSize)) & RDRAMSize)
+#define RSP_SegmentToPhysical( segaddr ) ((gSP.segment[(segaddr >> 24) & 0x0F] + (segaddr & 0x00FFFFFF)) & 0x00FFFFFF)
 
 void RSP_Init(void);
 void RSP_ProcessDList(void);
