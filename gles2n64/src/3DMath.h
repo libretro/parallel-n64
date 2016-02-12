06b49ce50369c6c7b4dc43613a3e8eb352e2633b
twinaphex <libretro@gmail.com>
2016-02-12 03:56:57 +0100

    Buildfix

--- gles2n64/src/3DMath.h
+++ gles2n64/src/3DMath.h
@@ -57,10 +57,6 @@ static INLINE float DotProduct(const float v0[3], const float v1[3])
    return v0[0]*v1[0] + v0[1]*v1[1] + v0[2]*v1[2];
 }
 
-#ifdef __NEON_OPT
-void MathInitNeon();
-#endif
-
 #ifdef __cplusplus
 }
 #endif
