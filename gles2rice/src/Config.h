522f931c5446249f17bb5818e3007f9a2f3ebf9a
twinaphex <libretro@gmail.com>
2016-02-12 03:14:00 +0100

    Cleanups

--- gles2rice/src/Config.h
+++ gles2rice/src/Config.h
@@ -21,7 +21,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define _RICE_CONFIG_H_
 
 #include <stdio.h>
-#include "../../libretro/boolean.h"
+
+#include <boolean.h>
 
 #include "typedefs.h"
 
