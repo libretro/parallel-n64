ea941be35f9bf315fe1dfdfd2f3e7e2aa6c4889d
twinaphex <libretro@gmail.com>
2016-02-12 03:34:36 +0100

    Cleanups

--- gles2n64/src/3DMath.h
+++ gles2n64/src/3DMath.h
@@ -1,18 +1,29 @@
 #ifndef _3DMATH_H
 #define _3DMATH_H
 
+#include <memory.h>
+#include <math.h>
 #include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define DotProduct gln64DotProduct
+void MultMatrix( float m0[4][4], float m1[4][4], float dest[4][4]);
 
-extern void (*MultMatrix)(float m0[4][4], float m1[4][4], float dest[4][4]);
-extern void (*TransformVectorNormalize)(float vec[3], float mtx[4][4]);
-extern void (*Normalize)(float v[3]);
-extern float (*DotProduct)(const float v0[3], const float v1[3]);
+void TransformVectorNormalize(float vec[3], float mtx[4][4]);
+
+static INLINE void Normalize(float v[3])
+{
+   float len = v[0]*v[0] + v[1]*v[1] + v[2]*v[2];
+   if (len != 0.0f)
+   {
+      len = sqrtf( len );
+      v[0] /= len;
+      v[1] /= len;
+      v[2] /= len;
+   }
+}
 
 static INLINE void CopyMatrix( float m0[4][4], float m1[4][4] )
 {
@@ -28,9 +39,7 @@ static INLINE void MultMatrix2( float m0[4][4], float m1[4][4] )
 
 static INLINE void Transpose3x3Matrix( float mtx[4][4] )
 {
-    float tmp;
-
-    tmp = mtx[0][1];
+    float tmp = mtx[0][1];
     mtx[0][1] = mtx[1][0];
     mtx[1][0] = tmp;
 
@@ -41,6 +50,11 @@ static INLINE void Transpose3x3Matrix( float mtx[4][4] )
     tmp = mtx[1][2];
     mtx[1][2] = mtx[2][1];
     mtx[2][1] = tmp;
+}
+
+static INLINE float DotProduct(const float v0[3], const float v1[3])
+{
+   return v0[0]*v1[0] + v0[1]*v1[1] + v0[2]*v1[2];
 }
 
 #ifdef __NEON_OPT
