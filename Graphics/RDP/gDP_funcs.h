ceb416e5b887866276d4a7aeaf4515cb9e82a574
twinaphex <libretro@gmail.com>
2016-03-29 08:41:06 +0200

    Update gDP_funcs.h

--- Graphics/RDP/gDP_funcs.h
+++ Graphics/RDP/gDP_funcs.h
@@ -9,7 +9,13 @@ enum gdp_plugin_type
    GDP_PLUGIN_GLN64
 };
 
-#define gDPSetScissor(mode, ulx, uly, lrx, lry) GDPSetScissor(GDP_PLUGIN_GLIDE64, mode, ulx, uly, lrx, lry)
+#ifdef GDP_PLUGIN
+#define GDP_DEF_PLUGIN GDP_PLUGIN
+#else
+#define GDP_DEF_PLUGIN GDP_PLUGIN_GLIDE64
+#endif
+
+#define gDPSetScissor(mode, ulx, uly, lrx, lry) GDPSetScissor(GDP_DEF_PLUGIN, mode, ulx, uly, lrx, lry)
 
 void GDPSetScissor(enum gdp_plugin_type plug_type, uint32_t mode, float ulx, float uly, float lrx, float lry );
 
