31e6892308c9c689b7c36241d705afd1fd5888ed
twinaphex <libretro@gmail.com>
2016-03-29 09:16:03 +0200

    Add documentation to gDPSetScissor

--- Graphics/RDP/gDP_funcs.cpp
+++ Graphics/RDP/gDP_funcs.cpp
@@ -1,6 +1,19 @@
 #include "gDP_funcs.h"
 
-void GDPSetScissor(enum gdp_plugin_type plug_type, uint32_t mode, float ulx, float uly, float lrx, float lry )
+/* Sets a scissoring box at the screen coordinates 
+ *
+ * mode              - Video mode:
+ *                     G_SC_NON_INTERLACE  - Draw all scan lines
+ *                     G_SC_ODD_INTERLACE  - Draw only odd-numbered scan lines
+ *                     G_SC_EVEN_INTERLACE - Draw only even-numbered scan lines
+ *
+ * ulx               - Screen's left edge coordinates (0.0~1023.75) 
+ * uly               - Screen's top edge coordinates (0.0~1023.75) 
+ * lrx               - Screen's right edge coordinates (0.0~1023.75) 
+ * lry               - Screen's bottom edge coordinates (0.0~1023.75) 
+ * */
+void GDPSetScissor(enum gdp_plugin_type plug_type, uint32_t mode,
+      float ulx, float uly, float lrx, float lry )
 {
    switch (plug_type)
    {
