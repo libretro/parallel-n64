d79e18bbd1bcb3c6a55094acd219a5b566e6917d
twinaphex <libretro@gmail.com>
2016-04-03 15:31:17 +0200

    (Glide64) Remove externs

--- glide2gl/src/Glide64/glide64_gDP.h
+++ glide2gl/src/Glide64/glide64_gDP.h
@@ -10,9 +10,6 @@
 extern "C" {
 #endif
 
-extern uint32_t ucode5_texshiftaddr;
-extern uint32_t ucode5_texshiftcount;
-extern uint16_t ucode5_texshift;
 extern int CI_SET;
 extern uint32_t swapped_addr;
 
