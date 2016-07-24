fae13f868802a7925ae6b72efab6567d357f893d
Tiny Tiger <hiddentigercrouchinglion@gmail.com>
2016-07-24 18:44:38 +0200

    Write out VACC to VD after testing VS and VT.

--- mupen64plus-rsp-cxd4/vu/vsubc.h
+++ mupen64plus-rsp-cxd4/vu/vsubc.h
@@ -22,11 +22,11 @@ INLINE static void set_bo(short* VD, short* VS, short* VT)
         dif[i] = (unsigned short)(VS[i]) - (unsigned short)(VT[i]);
     for (i = 0; i < N; i++)
         VACC_L[i] = VS[i] - VT[i];
-    vector_copy(VD, VACC_L);
     for (i = 0; i < N; i++)
         ne[i] = (VS[i] != VT[i]);
     for (i = 0; i < N; i++)
         co[i] = (dif[i] < 0);
+    vector_copy(VD, VACC_L);
     return;
 }
 
