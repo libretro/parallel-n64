2ce802dc0ec1fbdb65505799da67406b0054ecb9
twinaphex <libretro@gmail.com>
2016-12-06 03:51:01 +0100

    (angrylion) Backport https://sourceforge.net/p/angrylions-stuff/code/103

--- mupen64plus-video-angrylion/n64video.c
+++ mupen64plus-video-angrylion/n64video.c
@@ -8323,29 +8323,29 @@ no_read_zbuffer_coefficients:
         d_stwz_dyh[2]             = stw_info->d_stwz_dy[2] & ~0x000001FF;
         d_stwz_dyh[3]             = stw_info->d_stwz_dy[3] & ~0x000001FF;
 
-        stw_info->d_rgba_diff[0]  = d_rgba_deh[0] - d_rgba_dyh[0];
-        stw_info->d_rgba_diff[0] -= (stw_info->d_rgba_diff[0] >> 2);
+        stw_info->d_rgba_diff[0]  = d_rgba_deh[0] - (d_rgba_deh[0] >> 2) -
+           d_rgba_dyh[0] + (d_rgba_dyh[0] >> 2);
 
-        stw_info->d_rgba_diff[1]  = d_rgba_deh[1] - d_rgba_dyh[1];
-        stw_info->d_rgba_diff[1] -= (stw_info->d_rgba_diff[1] >> 2);
+        stw_info->d_rgba_diff[1]  = d_rgba_deh[1] - (d_rgba_deh[1] >> 2) -
+           d_rgba_dyh[1] + (d_rgba_dyh[1] >> 2);
 
-        stw_info->d_rgba_diff[2]  = d_rgba_deh[2] - d_rgba_dyh[2];
-        stw_info->d_rgba_diff[2] -= (stw_info->d_rgba_diff[2] >> 2);
+        stw_info->d_rgba_diff[2]  = d_rgba_deh[2] - (d_rgba_deh[2] >> 2) -
+           d_rgba_dyh[2] + (d_rgba_dyh[2] >> 2);
 
-        stw_info->d_rgba_diff[3]  = d_rgba_deh[3] - d_rgba_dyh[3];
-        stw_info->d_rgba_diff[3] -= (stw_info->d_rgba_diff[3] >> 2);
+        stw_info->d_rgba_diff[3]  = d_rgba_deh[3] - (d_rgba_deh[3] >> 2) -
+           d_rgba_dyh[3] + (d_rgba_dyh[3] >> 2);
 
-        stw_info->d_stwz_diff[0]  = d_stwz_deh[0] - d_stwz_dyh[0];
-        stw_info->d_stwz_diff[0] -= (stw_info->d_stwz_diff[0] >> 2);
+        stw_info->d_stwz_diff[0]  = d_stwz_deh[0] - (d_stwz_deh[0] >> 2) -
+           d_stwz_dyh[0] + (d_stwz_dyh[0] >> 2);
 
-        stw_info->d_stwz_diff[1]  = d_stwz_deh[1] - d_stwz_dyh[1];
-        stw_info->d_stwz_diff[1] -= (stw_info->d_stwz_diff[1] >> 2);
+        stw_info->d_stwz_diff[1]  = d_stwz_deh[1] - (d_stwz_deh[1] >> 2) -
+           d_stwz_dyh[1] + (d_stwz_dyh[1] >> 2);
 
-        stw_info->d_stwz_diff[2]  = d_stwz_deh[2] - d_stwz_dyh[2];
-        stw_info->d_stwz_diff[2] -= (stw_info->d_stwz_diff[2] >> 2);
+        stw_info->d_stwz_diff[2]  = d_stwz_deh[2] - (d_stwz_deh[2] >> 2) -
+           d_stwz_dyh[2] + (d_stwz_dyh[2] >> 2);
 
-        stw_info->d_stwz_diff[3]  = d_stwz_deh[3] - d_stwz_dyh[3];
-        stw_info->d_stwz_diff[3] -= (stw_info->d_stwz_diff[3] >> 2);
+        stw_info->d_stwz_diff[3]  = d_stwz_deh[3] - (d_stwz_deh[3] >> 2) -
+           d_stwz_dyh[3] + (d_stwz_dyh[3] >> 2);
     }
 
     if (other_modes.cycle_type == CYCLE_TYPE_COPY)
