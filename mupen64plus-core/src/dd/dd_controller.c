263bb16382d3e44181a3dd774e7ac74e088ba379
twinaphex <libretro@gmail.com>
2017-07-06 03:22:24 +0200

    Update

--- mupen64plus-core/src/dd/dd_controller.c
+++ mupen64plus-core/src/dd/dd_controller.c
@@ -134,156 +134,156 @@ int read_dd_regs(void* opaque, uint32_t address, uint32_t* value)
 
 int write_dd_regs(void* opaque, uint32_t address, uint32_t value, uint32_t mask)
 {
-    struct dd_controller* dd = (struct dd_controller*)opaque;
-    uint32_t reg = dd_reg(address);
+   uint8_t year, month, hour, day, min, sec;
+   struct dd_controller* dd = (struct dd_controller*)opaque;
+   uint32_t reg = dd_reg(address);
 
-    value &= 0xffff0000;
+   value &= 0xffff0000;
 
-    if (!ConfigGetParamBool(g_CoreConfig, "64DD"))
-        return 0;
+   if (!ConfigGetParamBool(g_CoreConfig, "64DD"))
+      return 0;
 
-    switch (reg)
-    {
-        case ASIC_DATA:
-            dd->regs[ASIC_DATA] = value;
-            break;
+   switch (reg)
+   {
+      case ASIC_DATA:
+         dd->regs[ASIC_DATA] = value;
+         break;
 
-        case ASIC_BM_STATUS_CTL:
-            /* SET SECTOR */
-            dd->regs[ASIC_CUR_SECTOR] = value & 0x00FF0000;
-            CUR_BLOCK = ((dd->regs[ASIC_CUR_SECTOR] >> 16) < 0x5A) ? 0 : 1;
+      case ASIC_BM_STATUS_CTL:
+         /* SET SECTOR */
+         dd->regs[ASIC_CUR_SECTOR] = value & 0x00FF0000;
+         CUR_BLOCK = ((dd->regs[ASIC_CUR_SECTOR] >> 16) < 0x5A) ? 0 : 1;
 
-            if (value & 0x01000000)
-            {
-                /* MECHA INT RESET */
-                dd->regs[ASIC_CMD_STATUS] &= ~0x02000000;
-            }
+         if (value & 0x01000000)
+         {
+            /* MECHA INT RESET */
+            dd->regs[ASIC_CMD_STATUS] &= ~0x02000000;
+         }
 
-            if (value & 0x02000000)
-            {
-                /* BLOCK TRANSFER */
-                dd->regs[ASIC_BM_STATUS_CTL] |= 0x01000000;
-            }
+         if (value & 0x02000000)
+         {
+            /* BLOCK TRANSFER */
+            dd->regs[ASIC_BM_STATUS_CTL] |= 0x01000000;
+         }
 
-            if (value & 0x10000000)
-            {
-                /* BM RESET */
-                dd_bm_reset_hold = 1;
-            }
-            
-            if (!(value & 0x10000000) && dd_bm_reset_hold)
-            {
-                /* BM RESET */
-                dd_bm_reset_hold = 0;
-                dd->regs[ASIC_CMD_STATUS] &= ~0x5C000000;
-                dd->regs[ASIC_BM_STATUS_CTL] = 0x00000000;
-                CUR_BLOCK = 0;
-                dd->regs[ASIC_CUR_SECTOR] = 0;
-            }
+         if (value & 0x10000000)
+         {
+            /* BM RESET */
+            dd_bm_reset_hold = 1;
+         }
 
-            if ((dd->regs[ASIC_CMD_STATUS] & 0x06000000) == 0)
-            {
-                g_cp0_regs[CP0_CAUSE_REG] &= ~0x00000800;
-                cp0_update_count();
-                check_interrupt();
-            }
+         if (!(value & 0x10000000) && dd_bm_reset_hold)
+         {
+            /* BM RESET */
+            dd_bm_reset_hold = 0;
+            dd->regs[ASIC_CMD_STATUS] &= ~0x5C000000;
+            dd->regs[ASIC_BM_STATUS_CTL] = 0x00000000;
+            CUR_BLOCK = 0;
+            dd->regs[ASIC_CUR_SECTOR] = 0;
+         }
 
-            if (value & 0x80000000)
-            {
-                /* BM START */
-                dd->regs[ASIC_BM_STATUS_CTL] |= 0x80000000;
-                dd_update_bm(dd);
-            }
-
-            break;
-
-        case ASIC_CMD_STATUS:
-            /* ASIC Commands */
-            timeinfo = (struct tm*)af_rtc_get_time(&g_dev.si.pif.af_rtc);
-            uint8_t year, month, hour, day, min, sec;
-
-            switch (value >> 16)
-            {
-                case 0x01:
-                    /* SEEK READ TRACK */
-                    dd->regs[ASIC_CUR_TK] = dd->regs[ASIC_DATA] | 0x60000000;
-                    dd->regs[ASIC_CMD_STATUS] &= ~0x00180000;
-                    dd_bm_mode_read = 1;
-                    dd_set_zone_and_track_offset(dd);
-                    break;
-
-                case 0x02:
-                    /* SEEK WRITE TRACK */
-                    dd->regs[ASIC_CUR_TK] = dd->regs[ASIC_DATA] | 0x60000000;
-                    dd->regs[ASIC_CMD_STATUS] &= ~0x00180000;
-                    dd_bm_mode_read = 0;
-                    dd_set_zone_and_track_offset(dd);
-                    break;
-
-                case 0x08:
-                    /* CLEAR DISK CHANGE FLAG */
-                    dd->regs[ASIC_CMD_STATUS] &= ~0x00010000;
-                    break;
-
-                case 0x09:
-                    /* CLEAR RESET FLAG */
-                    dd->regs[ASIC_CMD_STATUS] &= ~0x00400000;
-                    break;
-
-                case 0x12:
-                    /* Get Year/Month */
-
-                    /* Put time in DATA as BCD */
-                    year = (uint8_t)byte2bcd(timeinfo->tm_year);
-                    month = (uint8_t)byte2bcd((timeinfo->tm_mon + 1));
-
-                    dd->regs[ASIC_DATA] = (year << 24) | (month << 16);
-                    break;
-
-                case 0x13:
-                    /* Get Day/Hour */
-
-                    /* Put time in DATA as BCD */
-                    hour = (uint8_t)byte2bcd(timeinfo->tm_hour);
-                    day = (uint8_t)byte2bcd(timeinfo->tm_mday);
-
-                    dd->regs[ASIC_DATA] = (day << 24) | (hour << 16);
-                    break;
-                case 0x14:
-                    /* Get Min/Sec */
-
-                    /* Put time in DATA as BCD */
-                    min = (uint8_t)byte2bcd(timeinfo->tm_min);
-                    sec = (uint8_t)byte2bcd(timeinfo->tm_sec);
-
-                    dd->regs[ASIC_DATA] = (min << 24) | (sec << 16);
-                    break;
-
-                case 0x1b:
-                    /* Feature Inquiry */
-                    dd->regs[ASIC_DATA] = 0x00000000;
-                    break;
-            }
-
-            dd->regs[ASIC_CMD_STATUS] |= 0x02000000;
+         if ((dd->regs[ASIC_CMD_STATUS] & 0x06000000) == 0)
+         {
+            g_cp0_regs[CP0_CAUSE_REG] &= ~0x00000800;
             cp0_update_count();
-            g_cp0_regs[CP0_CAUSE_REG] |= 0x00000800;
             check_interrupt();
+         }
+
+         if (value & 0x80000000)
+         {
+            /* BM START */
+            dd->regs[ASIC_BM_STATUS_CTL] |= 0x80000000;
+            dd_update_bm(dd);
+         }
+
+         break;
+
+      case ASIC_CMD_STATUS:
+         /* ASIC Commands */
+         timeinfo = (struct tm*)af_rtc_get_time(&g_dev.si.pif.af_rtc);
+
+         switch (value >> 16)
+         {
+            case 0x01:
+               /* SEEK READ TRACK */
+               dd->regs[ASIC_CUR_TK] = dd->regs[ASIC_DATA] | 0x60000000;
+               dd->regs[ASIC_CMD_STATUS] &= ~0x00180000;
+               dd_bm_mode_read = 1;
+               dd_set_zone_and_track_offset(dd);
+               break;
+
+            case 0x02:
+               /* SEEK WRITE TRACK */
+               dd->regs[ASIC_CUR_TK] = dd->regs[ASIC_DATA] | 0x60000000;
+               dd->regs[ASIC_CMD_STATUS] &= ~0x00180000;
+               dd_bm_mode_read = 0;
+               dd_set_zone_and_track_offset(dd);
+               break;
+
+            case 0x08:
+               /* CLEAR DISK CHANGE FLAG */
+               dd->regs[ASIC_CMD_STATUS] &= ~0x00010000;
+               break;
+
+            case 0x09:
+               /* CLEAR RESET FLAG */
+               dd->regs[ASIC_CMD_STATUS] &= ~0x00400000;
+               break;
+
+            case 0x12:
+               /* Get Year/Month */
+
+               /* Put time in DATA as BCD */
+               year = (uint8_t)byte2bcd(timeinfo->tm_year);
+               month = (uint8_t)byte2bcd((timeinfo->tm_mon + 1));
+
+               dd->regs[ASIC_DATA] = (year << 24) | (month << 16);
+               break;
+
+            case 0x13:
+               /* Get Day/Hour */
+
+               /* Put time in DATA as BCD */
+               hour = (uint8_t)byte2bcd(timeinfo->tm_hour);
+               day = (uint8_t)byte2bcd(timeinfo->tm_mday);
+
+               dd->regs[ASIC_DATA] = (day << 24) | (hour << 16);
+               break;
+            case 0x14:
+               /* Get Min/Sec */
+
+               /* Put time in DATA as BCD */
+               min = (uint8_t)byte2bcd(timeinfo->tm_min);
+               sec = (uint8_t)byte2bcd(timeinfo->tm_sec);
+
+               dd->regs[ASIC_DATA] = (min << 24) | (sec << 16);
+               break;
+
+            case 0x1b:
+               /* Feature Inquiry */
+               dd->regs[ASIC_DATA] = 0x00000000;
+               break;
+         }
+
+         dd->regs[ASIC_CMD_STATUS] |= 0x02000000;
+         cp0_update_count();
+         g_cp0_regs[CP0_CAUSE_REG] |= 0x00000800;
+         check_interrupt();
 #if 0
-            add_interrupt_event(CART_INT, 1000);
+         add_interrupt_event(CART_INT, 1000);
 #endif
-            break;
+         break;
 
-        case ASIC_HARD_RESET:
-            dd->regs[ASIC_CMD_STATUS] |= 0x00400000;
-            break;
+      case ASIC_HARD_RESET:
+         dd->regs[ASIC_CMD_STATUS] |= 0x00400000;
+         break;
 
-        case ASIC_HOST_SECBYTE:
-            dd->regs[ASIC_HOST_SECBYTE] = value;
-            break;
-    }
+      case ASIC_HOST_SECBYTE:
+         dd->regs[ASIC_HOST_SECBYTE] = value;
+         break;
+   }
 
-    return 0;
+   return 0;
 }
 
 int dd_end_of_dma_event(struct dd_controller* dd)
