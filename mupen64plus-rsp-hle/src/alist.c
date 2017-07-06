b52b77721703f0238599e4f400d7d1de68050ecc
twinaphex <libretro@gmail.com>
2017-07-06 02:59:24 +0200

    Update

--- mupen64plus-rsp-hle/src/alist.c
+++ mupen64plus-rsp-hle/src/alist.c
@@ -21,9 +21,10 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
+
+#include <boolean.h>
 
 #include "alist.h"
 #include "arithmetics.h"
