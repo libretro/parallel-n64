d3e0c835cf6d034629f0aec5f7fced58305e8cc4
twinaphex <libretro@gmail.com>
2017-10-25 02:43:38 +0200

    Combine all RDP files

--- mupen64plus-video-angrylion-thr/core/common.h
+++ mupen64plus-video-angrylion-thr/core/common.h
@@ -11,15 +11,6 @@
 #define STRICTINLINE INLINE
 #endif
 
-// thread-local storage
-#if defined(_MSC_VER)
-    #define TLS __declspec(thread)
-#elif defined(__GNUC__)
-   #define TLS __thread
-#else
-    #define TLS _Thread_local // C11
-#endif
-
 #define GET_LOW(x)  (((x) & 0x3e) << 2)
 #define GET_MED(x)  (((x) & 0x7c0) >> 3)
 #define GET_HI(x)   (((x) >> 8) & 0xf8)
