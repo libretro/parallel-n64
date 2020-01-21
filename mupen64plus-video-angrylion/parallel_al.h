5956724af806071f7299db0ac82cbab0a6a4dbf0
twinaphex <libretro@gmail.com>
2020-01-21 10:40:10 +0100

    Update

--- mupen64plus-video-angrylion/parallel_al.h
+++ mupen64plus-video-angrylion/parallel_al.h
@@ -9,9 +9,12 @@ extern "C" {
 #define PARALLEL_MAX_WORKERS 64u
 
 void parallel_alinit(uint32_t num);
+
 void parallel_run(void task(uint32_t));
-uint32_t parallel_num_workers();
-void parallel_close();
+
+uint32_t parallel_num_workers(void);
+
+void parallel_close(void);
 
 #ifdef __cplusplus
 }
