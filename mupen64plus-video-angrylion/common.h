d2828dfe7d8f41b510efa9aa603cf4a9349531f5
twinaphex <libretro@gmail.com>
2020-01-21 09:54:05 +0100

    We use MSB_FIRST instead of LSB_FIRST

--- mupen64plus-video-angrylion/common.h
+++ mupen64plus-video-angrylion/common.h
@@ -1,23 +1,18 @@
 #pragma once
 
-// endianness
-#define LSB_FIRST 1 
-#ifdef LSB_FIRST
-    #define BYTE_ADDR_XOR       3
-    #define WORD_ADDR_XOR       1
-    #define BYTE4_XOR_BE(a)     ((a) ^ BYTE_ADDR_XOR)
-#else
+/* endianness */
+#ifdef MSB_FIRST
+    #define BYTE_XOR_DWORD_SWAP 4
+    #define WORD_XOR_DWORD_SWAP 2
     #define BYTE_ADDR_XOR       0
     #define WORD_ADDR_XOR       0
     #define BYTE4_XOR_BE(a)     (a)
-#endif
-
-#ifdef LSB_FIRST
+#else
+    #define BYTE_ADDR_XOR       3
+    #define WORD_ADDR_XOR       1
+    #define BYTE4_XOR_BE(a)     ((a) ^ BYTE_ADDR_XOR)
     #define BYTE_XOR_DWORD_SWAP 7
     #define WORD_XOR_DWORD_SWAP 3
-#else
-    #define BYTE_XOR_DWORD_SWAP 4
-    #define WORD_XOR_DWORD_SWAP 2
 #endif
 
 #define DWORD_XOR_DWORD_SWAP 1
