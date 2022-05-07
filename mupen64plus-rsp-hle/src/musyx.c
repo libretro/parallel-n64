be177d862cb776f3605c596eecfb2a6c35fe872f
mudlord <mudlord@protonmail.com>
2022-05-08 04:33:45 +1000

    apply twinaphex's requests.

--- mupen64plus-rsp-hle/src/musyx.c
+++ mupen64plus-rsp-hle/src/musyx.c
@@ -19,7 +19,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#include <stdbool.h>
+#include <boolean.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
