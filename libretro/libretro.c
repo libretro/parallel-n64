016f615acc6bf066eb5d0da70b2d5997c97add2f
Matt Pharoah <mtpharoah@gmail.com>
2023-05-10 19:04:46 -0400

    Bump version

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -877,7 +877,7 @@ void retro_set_environment(retro_environment_t cb)
 void retro_get_system_info(struct retro_system_info *info)
 {
    info->library_name = "ParaLLEl N64";
-   info->library_version = "2.9.0 (Parallel Launcher Edition)";
+   info->library_version = "2.10.0 (Parallel Launcher Edition)";
    info->valid_extensions = "n64|v64|z64|bin|u1|ndd";
    info->need_fullpath = false;
    info->block_extract = false;
